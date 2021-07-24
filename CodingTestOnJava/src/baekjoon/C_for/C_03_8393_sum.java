4a01ea85fa1858a28e5c48631bcdc9097c82aa59
Austin.MacbookPro <bellin83@gmail.com>
2021-07-25 08:26:23 +0900

    Add: codingtest
    문제 풀이
    
    https://www.acmicpc.net/problem/8393

--- CodingTestOnJava/src/baekjoon/C_for/C_03_8393_sum.java
+++ CodingTestOnJava/src/baekjoon/C_for/C_03_8393_sum.java
@@ -1,7 +1,17 @@
 package baekjoon.C_for;
 
+import java.util.Scanner;
+
 public class C_03_8393_sum {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lastNumber = sc.nextInt();
+        int sum = 0;
 
+        for (int i=1; i<= lastNumber; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
     }
 }
