ad3b46fa3b95e78005e8014096e2dbb6f4fa3e2c
Austin.MacbookPro <bellin83@gmail.com>
2021-07-25 15:52:39 +0900

    Add: codingTest printN

---
+++ CodingTestOnJava/src/baekjoon/C_for/C_05_prgintN.java
@@ -0,0 +1,20 @@
+package baekjoon.C_for;
+
+import java.io.*;
+
+public class C_05_prgintN {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i=1; i<=count; i++) {
+            bw.write(i + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
