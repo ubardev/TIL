6695104a24d886cd9a3676c0f1effd7a06184a29
Austin.MacbookPro <bellin83@gmail.com>
2021-07-25 16:27:12 +0900

    Add: codingTest

---
+++ CodingTestOnJava/src/baekjoon/C_for/C_06_2742_reverseN.java
@@ -0,0 +1,20 @@
+package baekjoon.C_for;
+
+import java.io.*;
+
+public class C_06_2742_reverseN {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i=N; i>0; i--) {
+            bw.write(i + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
