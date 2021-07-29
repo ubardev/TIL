5b5cfdf95ff482dbcd1c2afe2f47a47568a2047b
Austin.MacbookPro <bellin83@gmail.com>
2021-07-29 09:14:14 +0900

    :art: Add: codingTest

---
+++ CodingTestOnJava/src/baekjoon/E_array/E_02_2562_maxValue.java
@@ -0,0 +1,28 @@
+package baekjoon.E_array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class E_02_2562_maxValue {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int maxNumber = 0;
+        int maxNumberIndex = 0;
+
+        for (int i = 0; i < 9; i++) {
+            int number = Integer.parseInt(br.readLine());
+            if (maxNumber < number) {
+                maxNumber = number;
+                maxNumberIndex = i + 1;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(maxNumber)
+                .append("\n")
+                .append(maxNumberIndex);
+
+        System.out.println(sb);
+    }
+}
