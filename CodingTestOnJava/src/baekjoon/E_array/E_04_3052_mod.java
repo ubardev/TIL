cae392bcf870f79c5bef1f2ed9b7820007f830af
Austin.MacbookPro <bellin83@gmail.com>
2021-07-30 04:48:20 +0900

    :art: Fix: int array -> boolean array로 변경

--- CodingTestOnJava/src/baekjoon/E_array/E_04_3052_mod.java
+++ CodingTestOnJava/src/baekjoon/E_array/E_04_3052_mod.java
@@ -7,20 +7,15 @@ import java.io.InputStreamReader;
 public class E_04_3052_mod {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[42];
+        boolean[] arr = new boolean[42];
 
-        for (int i=0; i<42; i++) {
-            arr[i] = 0;
-        }
-
-        for (int i=0; i<10; i++) {
-            int num = Integer.parseInt(br.readLine());
-            arr[num % 42]++;
+        for (int i = 0; i < 10; i++) {
+            arr[Integer.parseInt(br.readLine()) % 42] = true;
         }
 
         int count = 0;
-        for (int i=0; i<42; i++) {
-            if (arr[i] > 0) count++;
+        for (boolean value : arr) {
+            if (value) count++;
         }
 
         System.out.println(count);
