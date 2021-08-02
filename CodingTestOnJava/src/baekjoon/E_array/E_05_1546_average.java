ed135465ce1d20b28ff326f7833de161b4de4835
Austin.MacbookPro <bellin83@gmail.com>
2021-08-03 06:13:57 +0900

    :art: Fix: 배열 안쓰도록 변경

--- CodingTestOnJava/src/baekjoon/E_array/E_05_1546_average.java
+++ CodingTestOnJava/src/baekjoon/E_array/E_05_1546_average.java
@@ -3,28 +3,27 @@ package baekjoon.E_array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class E_05_1546_average {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        double arr[] = new double[Integer.parseInt(br.readLine())];
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int max = -1;
+        double sum = 0.0;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if(max < value)
+                max = value;
+
+            sum += value;
         }
 
-        double sum = 0;
-        Arrays.sort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            sum += ((arr[i] / arr[arr.length - 1]) * 100);
-        }
-
-        System.out.println(sum / arr.length);
+        System.out.println((sum / max * 100) / N);
     }
 }
