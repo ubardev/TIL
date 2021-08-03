d81d9c8d40b04461dd8ef3896c8a8517dee881d0
Austin.MacbookPro <bellin83@gmail.com>
2021-08-04 07:00:23 +0900

    :art: Fix: practice

--- CodingTestOnJava/src/baekjoon/F_function/F_03_1065_hansu.java
+++ CodingTestOnJava/src/baekjoon/F_function/F_03_1065_hansu.java
@@ -1,38 +1,30 @@
 package baekjoon.F_function;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class F_03_1065_hansu {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int count = play(N);
-        System.out.println(count);
+        int number = Integer.parseInt(br.readLine());
+        int hanSuCount = getHansuCount(number);
+        System.out.println(hanSuCount);
     }
 
-    private static int play(int num) {
-        int count = 0;
+    private static int getHansuCount(int number) {
+        if (number < 100)
+            return number;
 
-        if (num < 100) {
-            return num;
-        } else {
-            count = 99;
+        int count = 99;
 
-            if (num == 1000) {
-                num = 999;
-            }
+        for (int i = 100; i <= number; i++) {
+            int hundred = i / 100;
+            int ten = (i / 10) % 10;
+            int one = i % 10;
 
-            for (int i = 100; i <= num; i++) {
-                int hun = i / 100;
-                int ten = (i / 10) % 10;
-                int one = i % 10;
-
-                if ((hun - ten) == (ten - one))
-                    count++;
-            }
+            if ((hundred - ten) == (ten - one))
+                count++;
         }
 
         return count;
