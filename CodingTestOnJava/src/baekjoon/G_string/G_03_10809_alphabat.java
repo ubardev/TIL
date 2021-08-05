d9ceb2b6d8b53a0836a2f3d87278c0230dfb7ccc
Austin.MacbookPro <bellin83@gmail.com>
2021-08-06 06:14:25 +0900

    :art: Add: codingTest

---
+++ CodingTestOnJava/src/baekjoon/G_string/G_03_10809_alphabat.java
@@ -0,0 +1,29 @@
+package baekjoon.G_string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class G_03_10809_alphabat {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+
+        Arrays.fill(arr, -1);
+
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (arr[ch - 'a'] == -1)
+                arr[ch - 'a'] = i;
+        }
+
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
