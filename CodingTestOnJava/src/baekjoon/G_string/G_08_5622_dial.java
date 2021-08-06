38a5e3b0c61a874d17d18d0a7c9afcda220f0f07
Austin.MacbookPro <bellin83@gmail.com>
2021-08-07 06:48:16 +0900

    :art: Add: 백준 5622 다이얼 풀이

---
+++ CodingTestOnJava/src/baekjoon/G_string/G_08_5622_dial.java
@@ -0,0 +1,28 @@
+package baekjoon.G_string;
+
+import java.io.IOException;
+
+public class G_08_5622_dial {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        int value;
+
+        while(true) {
+            value = System.in.read();
+
+            if (value == '\n')
+                break;
+
+            if (value < 68) count += 3;
+            else if (value < 71) count += 4;
+            else if (value < 74) count += 5;
+            else if (value < 77) count += 6;
+            else if (value < 80) count += 7;
+            else if (value < 84) count += 8;
+            else if (value < 87) count += 9;
+            else count += 10;
+        }
+
+        System.out.println(count);
+    }
+}
