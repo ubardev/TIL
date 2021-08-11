33167989c25f048273d35291514e2768bde67f1a
Austin.MacbookPro <bellin83@gmail.com>
2021-08-11 15:15:06 +0900

    :art: Add: 백준 2292

---
+++ CodingTestOnJava/src/baekjoon/H_basicMath1/H_01_2292_honeycomb.java
@@ -0,0 +1,25 @@
+package baekjoon.H_basicMath1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class H_01_2292_honeycomb {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 2;
+
+        if (N == 1)
+            System.out.println(1);
+        else {
+            while (range <= N) {
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.println(count);
+        }
+
+    }
+}
