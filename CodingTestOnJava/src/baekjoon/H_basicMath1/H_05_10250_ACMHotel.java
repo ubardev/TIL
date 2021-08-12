dfde58fda5bb32ba9bd441a911f08d43793dc950
Austin.MacbookPro <bellin83@gmail.com>
2021-08-13 06:39:37 +0900

    :art: Add: 백준 10250 호텔

---
+++ CodingTestOnJava/src/baekjoon/H_basicMath1/H_05_10250_ACMHotel.java
@@ -0,0 +1,37 @@
+package baekjoon.H_basicMath1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class H_05_10250_ACMHotel {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCaseCount = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < testCaseCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            getRoomInfo(st);
+        }
+    }
+
+    public static void getRoomInfo(StringTokenizer data) {
+        int height = Integer.parseInt(data.nextToken());
+        int width = Integer.parseInt(data.nextToken());
+        int N = Integer.parseInt(data.nextToken());
+
+        int floor;
+        int roomNumber;
+
+        if (N % height == 0) {
+            floor = height * 100;
+            roomNumber = N / height;
+        } else {
+            floor = (N % height) * 100;
+            roomNumber = (N / height) + 1;
+        }
+        System.out.println(floor + roomNumber);
+    }
+}
