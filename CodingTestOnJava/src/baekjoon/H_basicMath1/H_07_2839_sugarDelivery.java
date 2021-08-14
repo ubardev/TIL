29cfe7c5ddfdadea82fbdad09d464c5c5b37ae4c
Austin.MacbookPro <bellin83@gmail.com>
2021-08-15 06:27:06 +0900

    :art: Add: 백준 2839 설탕 배달

---
+++ CodingTestOnJava/src/baekjoon/H_basicMath1/H_07_2839_sugarDelivery.java
@@ -0,0 +1,26 @@
+package baekjoon.H_basicMath1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class H_07_2839_sugarDelivery {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while(true) {
+            if (N % 5 == 0) {
+                System.out.println(N / 5 + count);
+                break;
+            } else if (N < 0) {
+                System.out.println(-1);
+                break;
+            }
+
+            N -= 3;
+            count++;
+        }
+    }
+}
