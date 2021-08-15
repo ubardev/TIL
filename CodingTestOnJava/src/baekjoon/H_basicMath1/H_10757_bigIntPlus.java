1b663861e6b46232ed59ffab37bd4406799ed335
Austin.MacbookPro <bellin83@gmail.com>
2021-08-16 06:22:02 +0900

    :art: Fix: 백준 10757 수동 덧셈으로 수정

--- CodingTestOnJava/src/baekjoon/H_basicMath1/H_10757_bigIntPlus.java
+++ CodingTestOnJava/src/baekjoon/H_basicMath1/H_10757_bigIntPlus.java
@@ -3,7 +3,6 @@ package baekjoon.H_basicMath1;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class H_10757_bigIntPlus {
@@ -11,10 +10,37 @@ public class H_10757_bigIntPlus {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BigInteger A = new BigInteger(st.nextToken());
-        BigInteger B = new BigInteger(st.nextToken());
+        String str_A = st.nextToken();
+        String str_B = st.nextToken();
 
-        A = A.add(B);
-        System.out.println(A.toString());
+        int max_length = Math.max(str_A.length(), str_B.length());
+
+        int[] A = new int[max_length + 1];
+        int[] B = new int[max_length + 1];
+
+        for (int i = 0; i < str_A.length(); i++) {
+            A[i] = str_A.charAt((str_A.length() - 1) - i) - '0';
+        }
+
+        for (int i = 0; i < str_B.length(); i++) {
+            B[i] = str_B.charAt((str_B.length() - 1) - i) - '0';
+        }
+
+        for (int i = 0; i < max_length; i++) {
+            int value = A[i] + B[i];
+            A[i] = value % 10;
+            A[i + 1] += (value / 10);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        if (A[max_length] != 0)
+            sb.append(A[max_length]);
+
+        for (int i = max_length - 1; i >= 0; i--) {
+            sb.append(A[i]);
+        }
+
+        System.out.println(sb);
     }
 }
