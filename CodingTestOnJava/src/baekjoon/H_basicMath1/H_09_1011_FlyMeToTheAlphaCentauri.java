221eaf1d31fd6938231f1b1b7993bc58446a5ed0
Austin.MacbookPro <bellin83@gmail.com>
2021-08-17 07:02:16 +0900

    :art: Add: 백준 1011 풀이

--- CodingTestOnJava/src/baekjoon/H_basicMath1/H_09_1011_FlyMeToTheAlphaCentauri.java
+++ CodingTestOnJava/src/baekjoon/H_basicMath1/H_09_1011_FlyMeToTheAlphaCentauri.java
@@ -1,4 +1,36 @@
 package baekjoon.H_basicMath1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class H_09_1011_FlyMeToTheAlphaCentauri {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
+
+            int distance = Y - X;
+
+            int max = (int)Math.sqrt(distance);
+
+            if (max == Math.sqrt(distance)) {
+                sb.append(max * 2 - 1).append('\n');
+            } else if(distance <= max * max + max) {
+                sb.append(max * 2).append('\n');
+            } else {
+                sb.append(max * 2 + 1).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
