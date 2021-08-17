26266acd998b039e14c4a4e346933094ee8e6d87
Austin.MacbookPro <bellin83@gmail.com>
2021-08-18 06:14:46 +0900

    :art: Add: 백준 1978 소수

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_sosoo.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_sosoo.java
@@ -1,2 +1,32 @@
-package baekjoon.A_StepByStep.I_basicMath2;public class I_01_1978_sosoo {
+package baekjoon.A_StepByStep.I_basicMath2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class I_01_1978_sosoo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int count = 0;
+
+        for (int i = 0; i < T; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            if (isPrime(number)) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number == 1) return false;
+
+        for (int i = 2; i < number; i++) {
+            if (number % i ==0) return false;
+        }
+
+        return true;
+    }
 }
