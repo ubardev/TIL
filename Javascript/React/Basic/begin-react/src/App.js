7a72f91e48354f8e5c0d092de9617ea81d0158cf
Austin.MacbookPro <bellin83@gmail.com>
2021-08-18 07:15:17 +0900

    :art: Add: props

--- Javascript/React/Basic/begin-react/src/App.js
+++ Javascript/React/Basic/begin-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 
 function App() {
   return (
-    <div>
-      <Hello />
-    </div>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
