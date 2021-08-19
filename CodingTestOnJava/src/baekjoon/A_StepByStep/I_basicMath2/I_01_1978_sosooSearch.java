feaf72a7a866ceb8693fbd280b039ddb370cbd3d
bellin83 <bellin83@gmail.com>
2021-08-19 12:23:58 +0900

    Refactor: 파일명 변경

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_sosooSearch.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_sosooSearch.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class I_01_1978_sosoo {
+public class I_01_1978_sosooSearch {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
