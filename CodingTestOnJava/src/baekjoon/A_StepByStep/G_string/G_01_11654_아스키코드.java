8e33602f2069887926e5a1548fadacc070088543
Austin.MacbookPro <bellin83@gmail.com>
2021-08-25 06:07:48 +0900

    :recycle: Refactor: 파일명 변경

--- CodingTestOnJava/src/baekjoon/A_StepByStep/G_string/G_01_11654_아스키코드.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/G_string/G_01_11654_아스키코드.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class G_01_11654_asciiCode {
+public class G_01_11654_아스키코드 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int code = br.readLine().charAt(0);
