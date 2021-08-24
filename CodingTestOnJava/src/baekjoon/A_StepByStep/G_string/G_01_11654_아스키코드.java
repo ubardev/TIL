52e4150fb93f9d1b4916b062076bc162fcd6cd8e
Austin.MacbookPro <bellin83@gmail.com>
2021-08-25 06:15:03 +0900

    :art: Feat: 복습

--- CodingTestOnJava/src/baekjoon/A_StepByStep/G_string/G_01_11654_아스키코드.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/G_string/G_01_11654_아스키코드.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class G_01_11654_아스키코드 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int code = br.readLine().charAt(0);
-        System.out.println(code);
+        int result = br.readLine().charAt(0);
+        System.out.println(result);
     }
 }
