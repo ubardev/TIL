9d570d5142a91bb0ccdab8bbac2279e41381ea3d
Austin.MacbookPro <bellin83@gmail.com>
2021-08-25 06:22:04 +0900

    :recycle: Refactor: 파일명 변경
    git push

--- CodingTestOnJava/src/baekjoon/A_StepByStep/G_string/G_02_11720_숫자의합.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/G_string/G_02_11720_숫자의합.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class G_02_11720_sum {
+public class G_02_11720_숫자의합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
