25999f2b0d4c97580905b0d35880f91a7911079d
Austin.MacbookPro <bellin83@gmail.com>
2021-08-26 05:31:01 +0900

    :sparkles: Add: 백준 1085 직사각형에서탈출

---
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_07_1085_직사각형에서탈출.java
@@ -0,0 +1,22 @@
+package baekjoon.A_StepByStep.I_basicMath2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class I_07_1085_직사각형에서탈출 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int row = Math.min(x, w - x);
+        int height = Math.min(y, h - y);
+
+        System.out.println(Math.min(row, height));
+    }
+}
