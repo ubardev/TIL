3e98b7537026eaa7cd09dd7066f49faa3336f41a
Austin.MacbookPro <bellin83@gmail.com>
2021-08-26 05:20:52 +0900

    :recycle: Refactor: 파일명변경

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
@@ -5,14 +5,14 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class I_01_1978_primeSearch {
+public class I_01_1978_소수찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
         int count = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < N; i++) {
             int number = Integer.parseInt(st.nextToken());
             if (isPrime(number)) count++;
         }
@@ -24,9 +24,8 @@ public class I_01_1978_primeSearch {
         if (number == 1) return false;
 
         for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i ==0) return false;
+            if (number % i == 0) return false;
         }
-
         return true;
     }
 }
