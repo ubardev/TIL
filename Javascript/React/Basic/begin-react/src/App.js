1d7e77fadff2a69c61cb584f46344a0ade42f2c4
Austin.MacbookPro <bellin83@gmail.com>
2021-08-27 06:20:00 +0900

    :art: Add: useRef

--- Javascript/React/Basic/begin-react/src/App.js
+++ Javascript/React/Basic/begin-react/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { userRef } from 'react';
 import UserList from "./UserList";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: '일',
+      email: 'one@naver.com',
+    },
+    {
+      id: 2,
+      username: '이',
+      email: 'two@naver.com',
+    },
+    {
+      id: 3,
+      username: '삼',
+      email: 'three@naver.com',
+    },
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreact = () => {
+    console.log('nextId.current==========>', nextId.current);
+    nextId.current += 1;
+  }
+
   return (
-    <UserList />
+    <UserList users={users} />
   );
 }
 
--- Javascript/React/Basic/begin-react/src/UserList.js
+++ Javascript/React/Basic/begin-react/src/UserList.js
@@ -8,25 +8,7 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: '일',
-      email: 'one@naver.com',
-    },
-    {
-      id: 2,
-      username: '이',
-      email: 'two@naver.com',
-    },
-    {
-      id: 3,
-      username: '삼',
-      email: 'three@naver.com',
-    },
-  ];
-
+function UserList({ users }) {
   return (
     <div>
       {
