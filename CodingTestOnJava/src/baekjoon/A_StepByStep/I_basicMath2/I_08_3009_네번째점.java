0ba4e84a0baec7222c1f2a8296704ee829d279f3
Austin.MacbookPro <bellin83@gmail.com>
2021-08-27 05:12:24 +0900

    :bento: Feat: split으로 변경

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_08_3009_네번째점.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_08_3009_네번째점.java
@@ -3,44 +3,34 @@ package baekjoon.A_StepByStep.I_basicMath2;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class I_08_3009_네번째점 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int[] firstNumbers = new int[2];
-        st = new StringTokenizer(br.readLine(), " ");
-        firstNumbers[0] = Integer.parseInt(st.nextToken());
-        firstNumbers[1] = Integer.parseInt(st.nextToken());
+        String[] firstNumbers = br.readLine().split(" ");
+        String[] secondNumbers = br.readLine().split(" ");
+        String[] thirdNumbers = br.readLine().split(" ");
 
-        int[] secondNumbers = new int[2];
-        st = new StringTokenizer(br.readLine(), " ");
-        secondNumbers[0] = Integer.parseInt(st.nextToken());
-        secondNumbers[1] = Integer.parseInt(st.nextToken());
+        String x;
+        String y;
 
-        int[] thirdNumbers = new int[2];
-        st = new StringTokenizer(br.readLine(), " ");
-        thirdNumbers[0] = Integer.parseInt(st.nextToken());
-        thirdNumbers[1] = Integer.parseInt(st.nextToken());
-
-        if (firstNumbers[0] == secondNumbers[0]) {
-            System.out.print(thirdNumbers[0]);
-        } else if (firstNumbers[0] == thirdNumbers[0]) {
-            System.out.print(secondNumbers[0]);
+        if (firstNumbers[0].equals(secondNumbers[0])) {
+            x = thirdNumbers[0];
+        } else if (firstNumbers[0].equals(thirdNumbers[0])) {
+            x = secondNumbers[0];
         } else {
-            System.out.print(firstNumbers[0]);
+            x = firstNumbers[0];
         }
 
-        System.out.print(" ");
-
-        if (firstNumbers[1] == secondNumbers[1]) {
-            System.out.print(thirdNumbers[1]);
-        } else if (firstNumbers[1] == thirdNumbers[1]) {
-            System.out.print(secondNumbers[1]);
+        if (firstNumbers[1].equals(secondNumbers[1])) {
+            y = thirdNumbers[1];
+        } else if (firstNumbers[1].equals(thirdNumbers[1])) {
+            y = secondNumbers[1];
         } else {
-            System.out.print(firstNumbers[1]);
+            y = firstNumbers[1];
         }
+
+        System.out.println(x + " " + y);
     }
 }
