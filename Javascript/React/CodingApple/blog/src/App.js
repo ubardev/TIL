8954f30dd945be162bba698165faf26bdfcc6738
Austin.MacbookPro <bellin83@gmail.com>
2021-08-29 06:43:23 +0900

    :art: Add: state

--- Javascript/React/CodingApple/blog/src/App.js
+++ Javascript/React/CodingApple/blog/src/App.js
@@ -1,15 +1,24 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
+
   let posts = '강남 고기 맛집';
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ { color: 'blue', fontSize: '30px' } }>개발 Blog</div>
+        <div style={ { color: 'blue', fontSize: '30px' } }>
+          개발 Blog
+        </div>
       </div>
-      <h4>{ posts }</h4>
+      <div className="list">
+        <h3> { 글제목[0] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
