6bbcfd53ce50b6199d44238157b5042a11ce3d66
Austin.MacbookPro <bellin83@gmail.com>
2021-08-29 06:52:39 +0900

    :art: Feat: state변경

--- Javascript/React/CodingApple/blog/src/App.js
+++ Javascript/React/CodingApple/blog/src/App.js
@@ -1,21 +1,30 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집']);
-
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '남자 신발 추천', '남자 모자 추천']);
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ { color: 'blue', fontSize: '30px' } }>
-          개발 Blog
-        </div>
+        <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
