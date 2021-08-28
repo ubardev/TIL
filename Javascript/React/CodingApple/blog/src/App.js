1f6633e006a70c240a68b167e105f5cab8955148
Austin.MacbookPro <bellin83@gmail.com>
2021-08-29 08:45:23 +0900

    :art: Add: 모달창 추가

--- Javascript/React/CodingApple/blog/src/App.js
+++ Javascript/React/CodingApple/blog/src/App.js
@@ -6,12 +6,19 @@ import './App.css';
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '남자 신발 추천', '남자 모자 추천']);
   let [따봉, 따봉변경] = useState(0);
+
+  let [modal, modal변경] = useState(false);
+
   let posts = '강남 고기 맛집';
 
-  function 제목바꾸기() {
-    var newArray = [...글제목];
-    newArray[0] = '여자 코트 추천';
-    글제목변경( newArray );
+  // function 제목바꾸기() {
+  //   var newArray = [...글제목];
+  //   newArray[0] = '여자 코트 추천';
+  //   글제목변경( newArray );
+  // }
+
+  const modal토글 = () => {
+    modal변경(!modal);
   }
 
   return (
@@ -19,7 +26,6 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
         <p>2월 17일 발행</p>
@@ -27,17 +33,22 @@ function App() {
       </div>
       <div className="list">
         <h3> { 글제목[1] } </h3>
-        <p>2월 17일 발행</p>
+        <p>2월 18일 발행</p>
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목[2] } </h3>
-        <p>2월 17일 발행</p>
+        <h3 onClick={ () => { modal변경(true) } }> { 글제목[2] } </h3>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
 
-      <Modal />
+      <button onClick={ modal토글 }>버튼</button>
 
+      {
+        modal
+        ? <Modal />
+        : null
+      }
     </div>
   );
 }
