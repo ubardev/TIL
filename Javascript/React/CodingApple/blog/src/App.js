2afb97eaffc806304e727c77ca4f98b33fd6aa52
Austin.MacbookPro <bellin83@gmail.com>
2021-08-29 09:01:48 +0900

    :art: props

--- Javascript/React/CodingApple/blog/src/App.js
+++ Javascript/React/CodingApple/blog/src/App.js
@@ -6,40 +6,12 @@ import './App.css';
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '남자 신발 추천', '남자 모자 추천']);
   let [따봉, 따봉변경] = useState(0);
-
   let [modal, modal변경] = useState(false);
-
-  let posts = '강남 고기 맛집';
-
-  // function 제목바꾸기() {
-  //   var newArray = [...글제목];
-  //   newArray[0] = '여자 코트 추천';
-  //   글제목변경( newArray );
-  // }
-
-  const modal토글 = () => {
-    modal변경(!modal);
-  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
-      </div>
-      <div className="list">
-        <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3> { 글제목[1] } </h3>
-        <p>2월 18일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3 onClick={ () => { modal변경(true) } }> { 글제목[2] } </h3>
-        <p>2월 19일 발행</p>
-        <hr/>
       </div>
 
       {
@@ -52,22 +24,23 @@ function App() {
             </div>
           );
         })
-
       }
+
+      <button onClick={ () => modal변경(!modal) }>열고닫기</button>
 
       {
         modal
-        ? <Modal />
+        ? <Modal 글제목={글제목} />
         : null
       }
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{ props.글제목[1] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
