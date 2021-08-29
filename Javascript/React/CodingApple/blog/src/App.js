f1c3364b9ea342e65e8bff1dceebca1824680dd2
Austin.MacbookPro <bellin83@gmail.com>
2021-08-29 11:30:50 +0900

    :art: Add: props를 응용한 상세페이지 만들기

--- Javascript/React/CodingApple/blog/src/App.js
+++ Javascript/React/CodingApple/blog/src/App.js
@@ -7,6 +7,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '남자 신발 추천', '남자 모자 추천']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
 
   return (
     <div className="App">
@@ -15,10 +16,10 @@ function App() {
       </div>
 
       {
-        글제목.map((글) => {
+        글제목.map((글, i) => {
           return (
             <div className="list">
-              <h3> { 글 } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
+              <h3 onClick={() => { 누른제목변경(i) }}> { 글 } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
               <p>2월 18일 발행</p>
               <hr/>
             </div>
@@ -26,11 +27,15 @@ function App() {
         })
       }
 
+      {/* <button onClick={() => { 누른제목변경(0) }}>버튼1</button>
+      <button onClick={() => { 누른제목변경(1) }}>버튼2</button>
+      <button onClick={() => { 누른제목변경(2) }}>버튼3</button> */}
+
       <button onClick={ () => modal변경(!modal) }>열고닫기</button>
 
       {
         modal
-        ? <Modal 글제목={글제목} />
+        ? <Modal 글제목={글제목} 누른제목={누른제목} />
         : null
       }
     </div>
@@ -40,7 +45,7 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h2>{ props.글제목[1] }</h2>
+      <h2>{ props.글제목[props.누른제목] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
