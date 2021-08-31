7e806eebeab438d9ba48db2bdcdf076f81d3fbde
Austin.MacbookPro <bellin83@gmail.com>
2021-08-31 22:50:49 +0900

    :art: Add: input

--- Javascript/React/CodingApple/blog/src/App.js
+++ Javascript/React/CodingApple/blog/src/App.js
@@ -9,6 +9,8 @@ function App() {
   let [modal, modal변경] = useState(false);
   let [누른제목, 누른제목변경] = useState(0);
 
+  let [입력값, 입력값변경] = useState('');
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -18,7 +20,7 @@ function App() {
       {
         글제목.map((글, i) => {
           return (
-            <div className="list">
+            <div className="list" key={i}>
               <h3 onClick={() => { 누른제목변경(i) }}> { 글 } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 } </h3>
               <p>2월 18일 발행</p>
               <hr/>
@@ -27,9 +29,8 @@ function App() {
         })
       }
 
-      {/* <button onClick={() => { 누른제목변경(0) }}>버튼1</button>
-      <button onClick={() => { 누른제목변경(1) }}>버튼2</button>
-      <button onClick={() => { 누른제목변경(2) }}>버튼3</button> */}
+      {입력값}
+      <input onChange={ (e) => { 입력값변경(e.target.value) } } />
 
       <button onClick={ () => modal변경(!modal) }>열고닫기</button>
 
