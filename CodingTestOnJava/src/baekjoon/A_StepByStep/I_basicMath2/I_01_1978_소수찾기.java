5d20a67a9720d27f3087689c15d029ec0c52f035
austin783 <austin.hwang@tosslab.com>
2021-09-02 09:29:21 +0900

    :art: Fix: 다시 풀이

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
@@ -6,8 +6,27 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class I_01_1978_소수찾기 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+        int count = 0;
 
+        for (int i = 0; i < N; i++) {
+            int number = Integer.parseInt(stringTokenizer.nextToken());
+            if (isPrime(number)) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    private static boolean isPrime(int number) {
+        if (number == 1) return false;
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+
+        return true;
     }
 }
