cbec3b29c5868c41d0eb9055905ca3230222ea5c
austin783 <austin.hwang@tosslab.com>
2021-09-02 09:14:45 +0900

    :art: Feat: 쇼핑몰 메인

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
@@ -6,26 +6,8 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class I_01_1978_소수찾기 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < N; i++) {
-            int number = Integer.parseInt(st.nextToken());
-            if (isPrime(number)) count++;
-        }
-
-        System.out.println(count);
-    }
-
-    private static boolean isPrime(int number) {
-        if (number == 1) return false;
-
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
-        }
-        return true;
     }
 }
--- Javascript/React/CodingApple/shop/src/App.js
+++ Javascript/React/CodingApple/shop/src/App.js
@@ -1,11 +1,61 @@
+import React from 'react';
+import { Navbar, Container, Nav, NavDropdown, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Jumbotron className="background">
+        <h1>20% Season Off</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <h4>상품명</h4>
+            <h4>상품설명 & 가격</h4>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <h4>상품설명 & 가격</h4>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <h4>상품설명 & 가격</h4>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
