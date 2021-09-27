87663e0aba77774f93dbb76e8ae547c989f372f0
Ubar <bellin83@gmail.com>
2021-09-28 08:14:18 +0900

    :art: Add: 소수 찾기 복습

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
@@ -1,48 +1,40 @@
 package baekjoon.A_StepByStep.I_basicMath2;
 
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class I_01_1978_소수찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String numbers = br.readLine();
-        StringTokenizer st = new StringTokenizer(numbers, " ");
-
-
-        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int count = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        int[] numbers = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-
-        for (int i = 0; i < arr.length; i++) {
-            if (isPrime(arr[i])) count++;
+        for(int number : numbers) {
+            if (isFrime(number)) count++;
         }
 
         System.out.println(count);
     }
 
-    private static boolean isPrime(int number) {
-        boolean result = true;
-
+    private static boolean isFrime(int number) {
         if (number == 1) return false;
 
         for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) result = false;
+            if (number % i == 0) return false;
         }
 
-        return result;
+        return true;
     }
-//
-//
-//
-//
+
 //    package baekjoon.A_StepByStep.I_basicMath2;
 //
 //import java.io.IOException;
