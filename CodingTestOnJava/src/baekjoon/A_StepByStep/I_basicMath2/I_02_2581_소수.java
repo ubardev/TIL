c738d0a29515ad884850011b788b2783684bbf35
Ubar <bellin83@gmail.com>
2021-09-28 21:38:47 +0900

    :art: Feat: 소수 재풀이

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_02_2581_소수.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_02_2581_소수.java
@@ -5,24 +5,24 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class I_02_2581_소수 {
-    public static boolean prime[];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int min = Integer.parseInt(br.readLine());
+        int max = Integer.parseInt(br.readLine());
 
-        int M = Integer.parseInt(br.readLine());
-        int N = Integer.parseInt(br.readLine());
+        boolean[] primes = new boolean[max + 1];
 
-        prime = new boolean[N + 1];
-        get_prime();
+        getPrimes(primes);
 
         int sum = 0;
-        int min = Integer.MAX_VALUE;
-        for (int i = M; i <= N; i++) {
-            if (prime[i] == false) {
+        int minNumber = Integer.MAX_VALUE;
+
+        for (int i = min; i < primes.length; i++) {
+            if (!primes[i]) {
                 sum += i;
-                if (min == Integer.MAX_VALUE) {
-                    min = i;
+
+                if (minNumber == Integer.MAX_VALUE) {
+                    minNumber = i;
                 }
             }
         }
@@ -31,20 +31,72 @@ public class I_02_2581_소수 {
             System.out.println(-1);
         } else {
             System.out.println(sum);
-            System.out.println(min);
+            System.out.println(minNumber);
         }
     }
 
-    private static void get_prime() {
-        prime[0] = true;
-        prime[1] = true;
+    private static void getPrimes(boolean[] primes) {
+        primes[0] = true;
+        primes[1] = true;
 
-        for (int i = 2; i < Math.sqrt(prime.length); i++) {
-            if (prime[i]) continue;
+        for (int i = 2; i < Math.sqrt(primes.length); i++) {
+            if (primes[i]) continue;
 
-            for(int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
+            for (int j = i * i; j < primes.length; j += i) {
+                primes[j] = true;
             }
         }
     }
 }
+
+
+//package baekjoon.A_StepByStep.I_basicMath2;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.io.IOException;
+//
+//public class I_02_2581_소수 {
+//    public static boolean prime[];
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int M = Integer.parseInt(br.readLine());
+//        int N = Integer.parseInt(br.readLine());
+//
+//        prime = new boolean[N + 1];
+//        get_prime();
+//
+//        int sum = 0;
+//        int min = Integer.MAX_VALUE;
+//        for (int i = M; i <= N; i++) {
+//            if (prime[i] == false) {
+//                sum += i;
+//                if (min == Integer.MAX_VALUE) {
+//                    min = i;
+//                }
+//            }
+//        }
+//
+//        if (sum == 0) {
+//            System.out.println(-1);
+//        } else {
+//            System.out.println(sum);
+//            System.out.println(min);
+//        }
+//    }
+//
+//    private static void get_prime() {
+//        prime[0] = true;
+//        prime[1] = true;
+//
+//        for (int i = 2; i < Math.sqrt(prime.length); i++) {
+//            if (prime[i]) continue;
+//
+//            for(int j = i * i; j < prime.length; j += i) {
+//                prime[j] = true;
+//            }
+//        }
+//    }
+//}
