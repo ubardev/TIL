78e4269f6e925ba9c25f59589e93a1380e2e011e
Austin.MacbookPro <bellin83@gmail.com>
2021-10-11 08:20:52 +0900

    :art: 백준 1978 다시 복습

--- CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
+++ CodingTestOnJava/src/baekjoon/A_StepByStep/I_basicMath2/I_01_1978_소수찾기.java
@@ -8,74 +8,62 @@ import java.util.StringTokenizer;
 public class I_01_1978_소수찾기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int count = 0;
+        int count = Integer.parseInt(br.readLine());
+        int primeCount = 0;
 
-        int[] numbers = new int[N];
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < count; i++) {
+            int number = Integer.parseInt(stringTokenizer.nextToken());
+            if (isPrime(number)) {
+                primeCount++;
+            }
         }
 
-        for(int number : numbers) {
-            if (isFrime(number)) count++;
-        }
-
-        System.out.println(count);
+        System.out.println(primeCount);
     }
 
-    private static boolean isFrime(int number) {
+    private static boolean isPrime(int number) {
         if (number == 1) return false;
 
         for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) return false;
         }
-
         return true;
     }
+}
 
-//    package baekjoon.A_StepByStep.I_basicMath2;
+//package baekjoon.A_StepByStep.I_basicMath2;
 //
-//import java.io.IOException;
 //import java.io.BufferedReader;
 //import java.io.InputStreamReader;
+//import java.io.IOException;
 //import java.util.StringTokenizer;
 //
-//    public class I_01_1978_소수찾기 {
-//        public static void main(String[] args) throws IOException {
-//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//            int N = Integer.parseInt(br.readLine());
-//            String numbers = br.readLine();
-//            StringTokenizer st = new StringTokenizer(numbers, " ");
+//public class I_01_1978_소수찾기 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int count = Integer.parseInt(br.readLine());
+//        int primeCount = 0;
 //
+//        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
 //
-//            int[] arr = new int[N];
-//            int count = 0;
-//
-//            for (int i = 0; i < arr.length; i++) {
-//                arr[i] = Integer.parseInt(st.nextToken());
+//        for (int i = 0; i < count; i++) {
+//            int number = Integer.parseInt(stringTokenizer.nextToken());
+//            if (isPrime(number)) {
+//                primeCount++;
 //            }
-//
-//
-//            for (int i = 0; i < arr.length; i++) {
-//                if (isPrime(arr[i])) count++;
-//            }
-//
-//            System.out.println(count);
 //        }
 //
-//        private static boolean isPrime(int number) {
-//            boolean result = true;
-//
-//            if (number == 1) return false;
-//
-//            for (int i = 2; i <= Math.sqrt(number); i++) {
-//                if (number % i == 0) result = false;
-//            }
-//
-//            return result;
-//        }
+//        System.out.println(primeCount);
 //    }
-
-}
+//
+//    private static boolean isPrime(int number) {
+//        if (number == 1) return false;
+//
+//        for (int i = 2; i <= Math.sqrt(number); i++) {
+//            if (number % i == 0) return false;
+//        }
+//        return true;
+//    }
+//}
