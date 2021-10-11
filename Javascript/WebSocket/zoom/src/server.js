abade68b30862fc427cccf6963171a7566b28293
Austin.MacbookPro <bellin83@gmail.com>
2021-10-12 08:02:31 +0900

    :art: Add: 서로 다른 브라우저에서 메세지 주고 받기

--- Javascript/WebSocket/zoom/src/server.js
+++ Javascript/WebSocket/zoom/src/server.js
@@ -15,15 +15,17 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Brower ✅");
   socket.on("close", () => {
     console.log("Disconnected from the Browser ❌");
   });
   socket.on("message", (message) => {
-    console.log(message.toString('utf8'));
-  })
-  socket.send("hello!!");
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
 });
 
 server.listen(3000, handleListen);
