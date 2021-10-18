ebf46e9490ba9dae0b17ca3f07263f9aeef6f452
Austin.MacbookPro <bellin83@gmail.com>
2021-10-19 05:30:43 +0900

    :art: Add: Room Nitifications 8min

--- Javascript/WebSocket/zoom/src/public/js/app.js
+++ Javascript/WebSocket/zoom/src/public/js/app.js
@@ -15,11 +15,21 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  socket.emit("new_message", input.value);
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit, () => {
+    addMessage(`You: ${input.value}`);
+  });
 }
 
 function handleRoomSubmit(event) {
@@ -34,4 +44,8 @@ form.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", () => {
   addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
 });
--- Javascript/WebSocket/zoom/src/server.js
+++ Javascript/WebSocket/zoom/src/server.js
@@ -23,6 +23,14 @@ wsServer.on("connection", (socket) => {
     done();
     socket.to(roomName).emit("welcome");
   });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach(room => socket.to(room).emit("bye"));
+  })
+
+  socket.on("new_message", (meg, done) => {
+
+  });
 });
 
 
