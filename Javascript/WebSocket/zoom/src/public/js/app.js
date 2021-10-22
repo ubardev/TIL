656e547a20f6eb3698254985b708fc6a8c194937
Ubar <bellin83@gmail.com>
2021-10-22 09:44:49 +0900

    :art: Add: Room Count part Two

--- Javascript/WebSocket/zoom/src/public/js/app.js
+++ Javascript/WebSocket/zoom/src/public/js/app.js
@@ -63,3 +63,19 @@ socket.on("bye", (left) => {
 });
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+  const roomList = welcome.querySelector("ul");
+
+  roomList.innerText = "";
+
+  if (rooms.length === 0) {
+    return;
+  }
+
+  rooms.forEach(room => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(li);
+  })
+});
--- Javascript/WebSocket/zoom/src/server.js
+++ Javascript/WebSocket/zoom/src/server.js
@@ -40,10 +40,15 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
 
   socket.on("disconnecting", () => {
     socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+  });
+
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
 
   socket.on("new_message", (msg, room, done) => {
