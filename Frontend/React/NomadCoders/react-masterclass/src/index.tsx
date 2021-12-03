811e17bf4fce1c0f4286f1c3e3adec43a530df58
Ubar <bellin83@gmail.com>
2021-12-03 15:33:05 +0900

    :art: 3.6 Themes

--- Frontend/React/NomadCoders/react-masterclass/src/index.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={ darkTheme }>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
