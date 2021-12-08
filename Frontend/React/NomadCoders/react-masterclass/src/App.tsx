0b84778a8d66547f71b8caedaabafcbac984e9a6
austin783 <austin.hwang@tosslab.com>
2021-12-09 08:59:19 +0900

    :art: 4.0 Setup

--- Frontend/React/NomadCoders/react-masterclass/src/App.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/App.tsx
@@ -1,20 +1,7 @@
-import styled, { keyframes } from "styled-components";
-import { useState } from "react";
-
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+  return <Router />
 }
 
 export default App;
