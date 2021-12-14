e34e6ddcfe2b724cc5096e2946e4313faed8886a
Ubar <bellin83@gmail.com>
2021-12-14 09:20:09 +0900

    :art: 47. 이벤트 리스닝 및 이벤트 핸들러 수행하기

--- Frontend/React/Udemy/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Frontend/React/Udemy/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,8 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => console.log('clicked!');
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={ props.date } />
@@ -10,6 +12,7 @@ const ExpenseItem = (props) => {
         <h2>{ props.title }</h2>
         <div className="expense-item__price">${ props.amount }</div>
       </div>
+      <button onClick={ clickHandler }>Change Title</button>
     </Card>
   );
 }
