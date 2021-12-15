85ddfc0cc439694411da4e1fb7d348e45b8aacfe
ubardev <bellin83@gmail.com>
2021-12-16 08:42:23 +0900

    :art: 58. 양방향 바인딩 추가

--- Frontend/React/Udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ Frontend/React/Udemy/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -50,6 +50,10 @@ const ExpenseForm = () => {
     };
 
     console.log('expenseData ==========> ', expenseData);
+
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -57,15 +61,30 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="mew-expense__actions">
