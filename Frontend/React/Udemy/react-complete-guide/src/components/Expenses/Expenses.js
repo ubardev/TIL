315332cc280508cb830766fdb5076888b6d7bb0b
Ubar <bellin83@gmail.com>
2021-12-17 07:56:14 +0900

    :art: 62. 연습하기 : 이벤트 및 State 작업하기 (정답)

--- Frontend/React/Udemy/react-complete-guide/src/components/Expenses/Expenses.js
+++ Frontend/React/Udemy/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,26 +1,42 @@
+import React, { useState } from 'react';
+
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    // console.log('Expenses.js', selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem 
-        title={ items[0].title }
-        amount={ items[0].amount }
-        date={ items[0].date } 
-      />
-      <ExpenseItem 
-        title={ items[1].title }
-        amount={ items[1].amount }
-        date={ items[1].date } 
-      />
-      <ExpenseItem 
-        title={ items[2].title }
-        amount={ items[2].amount }
-        date={ items[2].date } 
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={ filteredYear }
+          onChangeFilter={ filterChangeHandler }
+        />
+        <ExpenseItem 
+          title={ items[0].title }
+          amount={ items[0].amount }
+          date={ items[0].date } 
+        />
+        <ExpenseItem 
+          title={ items[1].title }
+          amount={ items[1].amount }
+          date={ items[1].date } 
+        />
+        <ExpenseItem 
+          title={ items[2].title }
+          amount={ items[2].amount }
+          date={ items[2].date } 
+        />
+      </Card>
+    </div>
   );
 };
 
