f18936d08e9d9054594116cd44c5fa47b54da0e2
ubardev <bellin83@gmail.com>
2021-12-16 09:16:32 +0900

    :art: 59. 자식 대 부모 컴포넌트 통신(상향식)

--- Frontend/React/Udemy/react-complete-guide/src/App.js
+++ Frontend/React/Udemy/react-complete-guide/src/App.js
@@ -26,12 +26,16 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js ==========> ', expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={ expenses } />
     </div>
   );
 }
 
-export default App;  
+export default App;
