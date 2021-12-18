3a78bf78f3bd4fb076d0ca528455a315cdda7ca9
Ubar <bellin83@gmail.com>
2021-12-19 07:22:30 +0900

    :art: 4.5 Coin Data

--- Frontend/React/NomadCoders/react-masterclass/src/routes/Coin.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useParams } from "react-router";
 import styled from "styled-components";
@@ -36,6 +36,25 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<Params>();
   const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (
+      async() => {
+        const infoData = await(
+          await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+        ).json();
+
+        const priceData = await(
+          await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+        ).json();
+
+        setInfo(infoData);
+        setPriceInfo(priceData);
+      }
+    )();
+  }, []);
 
   return (
     <Container>
