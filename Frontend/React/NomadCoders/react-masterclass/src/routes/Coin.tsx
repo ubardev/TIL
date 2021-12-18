f93627ec1154fc2100c993219775a3b7f3d01edc
Ubar <bellin83@gmail.com>
2021-12-19 07:05:19 +0900

    :art: 4.4 Route States
    git push

--- Frontend/React/NomadCoders/react-masterclass/src/routes/Coin.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/routes/Coin.tsx
@@ -1,14 +1,54 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { useParams } from "react-router";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.div`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 interface Params {
   coinId: string;
 }
 
-function Coin() {
-  const { coinId } = useParams<Params>();
-  console.log('params ==========> ', coinId);
+interface RouteState {
+  name: string;
+}
 
-  return <h1>Coin: { coinId }</h1>
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<Params>();
+  const { state } = useLocation<RouteState>();
+
+  return (
+    <Container>
+      <Header>
+        <Title>{ state?.name || "Loading.." }</Title>
+      </Header>
+      {
+        loading
+          ? <Loader>Loading...</Loader>
+          : null
+      }
+    </Container>
+  );
 }
 
 export default Coin;
