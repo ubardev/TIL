d3ba80a865e0514d1567ce36b0d2064a5debba16
Ubar <bellin83@gmail.com>
2021-12-23 23:21:12 +0900

    :art: 4.9 React Query part One

--- Frontend/React/NomadCoders/react-masterclass/src/routes/Coins.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/routes/Coins.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
-
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container = styled.div`
   padding: 0 20px;
@@ -50,7 +51,7 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string,
   name: string,
   symbol: string,
@@ -61,17 +62,7 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    (async() => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
 
   return (
     <Container>
@@ -79,11 +70,11 @@ function Coins() {
         <Title>코인</Title>
       </Header>
       {
-        loading
+        isLoading
           ? <Loader>Loading...</Loader>
           : (
             <CoinsList>
-              { coins.map(coin => (
+              { data?.slice(0, 100).map(coin => (
                 <Coin key={coin.id}>
                   <Link
                     to={{
