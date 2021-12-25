ba884719dfe144a112b007d7747e067d55517306
Ubar <bellin83@gmail.com>
2021-12-26 07:22:55 +0900

    :art: 4.13 Price Chart part Two

--- Frontend/React/NomadCoders/react-masterclass/src/api.ts
+++ Frontend/React/NomadCoders/react-masterclass/src/api.ts
@@ -20,7 +20,7 @@ export function fetchCoinTickers(coinId: string) {
 
 export function fetchCoinHistory(coinId:string) {
   const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 24 * 7;
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
 
   return fetch(
     `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
