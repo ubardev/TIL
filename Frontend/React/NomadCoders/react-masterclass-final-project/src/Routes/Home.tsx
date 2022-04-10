6f39031949e86c538c9f2c0865f63e86d3929a8e
Ubar <bellin83@gmail.com>
2022-04-10 15:58:29 +0900

    :sparkles: 8.10 Box Animations part Two

--- Frontend/React/NomadCoders/react-masterclass-final-project/src/Routes/Home.tsx
+++ Frontend/React/NomadCoders/react-masterclass-final-project/src/Routes/Home.tsx
@@ -65,6 +65,19 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth + 5,
@@ -83,14 +96,25 @@ const boxVariants = {
   },
   hover: {
     scale: 1.3,
-    y: -50,
+    y: -80,
     transition: {
       delay: 0.5,
-      duaration: 0.3,
+      duaration: 0.1,
       type: "tween",
     },
   },
 };
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duaration: 0.1,
+      type: "tween",
+    },
+  },
+}
 
 const offset = 6;
 
@@ -146,7 +170,11 @@ function Home() {
                       variants={boxVariants}
                       transition={{ type: "tween" }}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                    />
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
