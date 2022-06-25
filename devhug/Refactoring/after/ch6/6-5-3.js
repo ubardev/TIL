ddbdbf53eb999ded65b51e4c094a72e60ece0183
Ubar <bellin83@gmail.com>
2022-06-25 15:34:48 +0900

    :sparkles: 6.10 함수 선언 바꾸기 (책 6.5)

--- devhug/Refactoring/after/ch6/6-5-3.js
+++ devhug/Refactoring/after/ch6/6-5-3.js
@@ -1,3 +1,3 @@
-export function inNewEngland(aCustomer) {
-  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+export function inNewEngland(state) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(state);
 }
