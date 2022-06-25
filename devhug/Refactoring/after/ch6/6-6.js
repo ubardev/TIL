8f79d4e6d1c3b989108110023c897a638ba7fc0b
Ubar <bellin83@gmail.com>
2022-06-26 06:13:23 +0900

    :sparkles: 6.14 변수 캡슐화하기 (책 6.6)

--- devhug/Refactoring/after/ch6/6-6.js
+++ devhug/Refactoring/after/ch6/6-6.js
@@ -1,4 +1,22 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #lastName;
+  #firstName;
+
+  constructor(data) {
+    this.#lastName = data.lastName;
+    this.#firstName = data.firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: "마틴", lastName: "파울러" });
 
 export function getDefaultOwner() {
   return defaultOwner;
