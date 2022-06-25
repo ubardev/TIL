2974f6d6c91cb5f266160edf9b246959652cee5a
Ubar <bellin83@gmail.com>
2022-06-26 06:26:37 +0900

    :sparkles: 6.15 여러 함수를 클래스로 묶기 (책 6.9)

--- devhug/Refactoring/after/ch6/6-9-client2.js
+++ devhug/Refactoring/after/ch6/6-9-client2.js
@@ -1,10 +1,4 @@
-import { acquireReading, baseRate } from './6-9.js';
+import { acquireReading } from "./6-9.js";
 
-const aReading = acquireReading();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-
-function taxThreshold(year) {
-  return 0.1;
-}
-
-export const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const reading = acquireReading();
+export const taxableCharge = reading.taxableCharge;
--- devhug/Refactoring/after/ch6/6-9.js
+++ devhug/Refactoring/after/ch6/6-9.js
@@ -1,10 +1,57 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+export class Reading {
+  #customer;
+  #quantity;
+  #month;
+  #year;
+
+  constructor(data) {
+    this.#customer = data.customer;
+    this.#quantity = data.quantity;
+    this.#month = data.month;
+    this.#year = data.year;
+  }
+
+  get customer() {
+    return this.#customer;
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  get month() {
+    return this.#month;
+  }
+
+  get year() {
+    return this.#year;
+  }
+
+  get baseRate() {
+    if (this.#year === 2017 && this.#month === 5) return 0.1;
+    return 0.2;
+  }
+
+  get baseCharge() {
+    return this.baseRate * this.quantity;
+  }
+
+  get taxThreshold() {
+    return 0.1;
+  }
+
+  get taxableCharge() {
+    return Math.max(0, this.baseCharge - this.taxThreshold);
+  }
+}
+
+const reading = new Reading({
+  customer: "ivan",
+  quantity: 10,
+  month: 5,
+  year: 2017,
+});
 
 export function acquireReading() {
   return reading;
-}
-
-export function baseRate(month, year) {
-  if (year === 2017 && month === 5) return 0.1;
-  return 0.2;
 }
