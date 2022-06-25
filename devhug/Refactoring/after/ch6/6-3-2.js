97290fd2b610de93ad80ef09c8091ff6e8fc937d
Ubar <bellin83@gmail.com>
2022-06-25 15:21:43 +0900

    :sparkles: 6.6 변수 추출하기 (책 6.3)

--- devhug/Refactoring/after/ch6/6-3-2.js
+++ devhug/Refactoring/after/ch6/6-3-2.js
@@ -1,20 +1,29 @@
 export class Order {
+  #data;
   constructor(aRecord) {
-    this._data = aRecord;
+    this.#data = aRecord;
   }
 
   get quantity() {
-    return this._data.quantity;
+    return this.#data.quantity;
   }
   get itemPrice() {
-    return this._data.itemPrice;
+    return this.#data.itemPrice;
   }
 
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    return this.basePrice - this.discount + this.shipping;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+
+  get discount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
   }
 }
