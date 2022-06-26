b190813269043e7a9a1dc10f141546ebb616cd98
Ubar <bellin83@gmail.com>
2022-06-27 01:24:03 +0900

    :sparkles: .

--- devhug/Refactoring/after/ch6/6-10.js
+++ devhug/Refactoring/after/ch6/6-10.js
@@ -1,7 +1,20 @@
-const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+import _ from "lodath";
+
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 export function acquireReading() {
   return reading;
+}
+
+export function enrichReading(original) {
+  const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(0, this.baseCharge - this.taxThreshold);
+  return result;
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity;
 }
 
 export function baseRate(month, year) {
