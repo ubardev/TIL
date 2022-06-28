35b15f06c410cb1c64b3841bf559ddb403334e90
Ubar <bellin83@gmail.com>
2022-06-29 08:31:22 +0900

    :sparkles: 7.11 알고리즘 교체하기 (책 7.9)

--- devhug/Refactoring/after/ch7/7-9.js
+++ devhug/Refactoring/after/ch7/7-9.js
@@ -1,19 +1,9 @@
 function foundPerson(people) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-  return '';
+  const candidates = ["Don", "John", "Kent"];
+  return people.find((p) => candidates.includes(p)) || "";
 }
 
-console.log(foundPerson(['John']));
-console.log(foundPerson(['Don', 'John']));
-console.log(foundPerson(['Kent', 'Don', 'John']));
-console.log(foundPerson(['Lisa', 'Don', 'Tom']));
+console.log(foundPerson(["John"]));
+console.log(foundPerson(["Don", "John"]));
+console.log(foundPerson(["Kent", "Don", "John"]));
+console.log(foundPerson(["Lisa", "Don", "Tom"]));
