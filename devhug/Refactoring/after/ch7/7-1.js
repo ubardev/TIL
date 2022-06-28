3d649d00ca516b7fa007b2d43097de62d627632c
Ubar <bellin83@gmail.com>
2022-06-28 13:09:21 +0900

    :sparkles: 7.4 컬렉션 캡슐화하기 (책 7.2)

--- devhug/Refactoring/after/ch7/7-1.js
+++ devhug/Refactoring/after/ch7/7-1.js
@@ -1,39 +1,4 @@
-class Organization {
-  #data;
-  #name;
-  #country;
-
-  constructor(data) {
-    this.#data = data;
-    this.#name = data.name;
-    this.#country = data.country;
-  }
-
-  get name() {
-    return this.#name;
-  }
-
-  set name(value) {
-    this.#name = value;
-  }
-
-  get country() {
-    return this.#country;
-  }
-
-  set country(value) {
-    this.#country = value;
-  }
-
-  get rowData() {
-    return { ...this.#data } // 얕은 복사, lodash의 cloneDeep 이용해 깊은 복사 사용
-    // return { name: this.name, country: this.country  }
-  }
-}
-
-const organization = new Organization({
-  name: 'Acme Gooseberries', country: 'GB'
-});
+const organization = { name: 'Acme Gooseberries', country: 'GB' };
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
