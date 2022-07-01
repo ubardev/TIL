cdd88e7c9709bb23c3174beda6813705256a37d0
Ubar <bellin83@gmail.com>
2022-07-01 17:04:30 +0900

    :sparkles: 9.4 파생 변수를 질의 함수로 바꾸기 (책 9.3)

--- devhug/Refactoring/after/ch9/9-3.js
+++ devhug/Refactoring/after/ch9/9-3.js
@@ -2,12 +2,10 @@
 class Order {
   // 다른 코드 있다고 가정
   get discountedTotal() {
-    return this._discountedTotal;
+    return this._basePrice - this.discount;
   }
   set discount(value) {
-    const old = this._discount;
     this._discount = value;
-    this._discountedTotal += old - value;
   }
 }
 
@@ -15,10 +13,9 @@ class Order {
 class ProductionPlan {
   // 다른 코드 있다고 가정
   get production() {
-    return this._production;
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
   }
 }
