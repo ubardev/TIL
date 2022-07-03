4e9ce2176b07925d83471ceda93a06a7a5f535d1
Ubar <bellin83@gmail.com>
2022-07-03 23:13:32 +0900

    :sparkles: 10.3 조건식 통합하기 (책 10.2)

--- devhug/Refactoring/after/ch10/10-2.js
+++ devhug/Refactoring/after/ch10/10-2.js
@@ -1,6 +1,14 @@
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
+  function isNotEligibleForDisability() {
+    return (
+      employee.seniority < 2 ||
+      employee.monthsDisabled > 12 ||
+      employee.isPartTime
+    );
+  }
+
+  if (isNotEligibleForDisability(employee)) {
+    return 0;
+  }
   return 1;
 }
