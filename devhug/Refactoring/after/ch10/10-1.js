c546f2eb4a31b6ce4707c33c45f45705e774d570
Ubar <bellin83@gmail.com>
2022-07-03 12:45:57 +0900

    :sparkles: 10.2 조건문 분해하기 (책 10.1)

--- devhug/Refactoring/after/ch10/10-1.js
+++ devhug/Refactoring/after/ch10/10-1.js
@@ -1,7 +1,15 @@
 function calculateCharge(date, quantity, plan) {
-  let charge = 0;
-  if (!date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd))
-    charge = quantity * plan.summerRate;
-  else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-  return charge;
+  return isSummer() ? summerCharge() : regularCharge();
+
+  function isSummer() {
+    return !date.isBefore(plan.summerStart) && !date.isAfter(plan.summerEnd);
+  }
+
+  function summerCharge() {
+    return quantity * plan.summerRate;
+  }
+
+  function regularCharge() {
+    return quantity * plan.regularRate + plan.regularServiceCharge;
+  }
 }
