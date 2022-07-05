6dc99f96ce1900c83f6b2bd973e035b41a102c8c
Ubar <bellin83@gmail.com>
2022-07-05 16:09:39 +0900

    :sparkles: 11.4 플래그 인수 제거하기(책 11.3)

--- devhug/Refactoring/after/ch11/11-3.js
+++ devhug/Refactoring/after/ch11/11-3.js
@@ -1,19 +1,19 @@
 // 예제 1
-function setDimension(name, value) {
-  if (name === 'height') {
-    this._height = value;
-    return;
-  }
-  if (name === 'width') {
-    this._width = value;
-    return;
-  }
+function wetWidth(value) {
+  this._width = value;
+}
+
+function wetHeight(value) {
+  this._height = value;
 }
 
 // 예제 2
 class Concert {
-  book(customer, isPremium) {}
+  regularBook(customer) {}
+  premiumBook(customer) {}
+  #book(customer, isPremium) {}
 }
 
 // 예제 3
-function setSwitch(on);
+function switchOn();
+function switchOff();
