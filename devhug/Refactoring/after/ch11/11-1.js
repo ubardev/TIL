7142bb588868f7fb9f0f7d0bec30504c80821263
Ubar <bellin83@gmail.com>
2022-07-05 15:46:35 +0900

    :sparkles: 11.2 질의 함수와 변경 함수 분리하기(책 11.1)

--- devhug/Refactoring/after/ch11/11-1.js
+++ devhug/Refactoring/after/ch11/11-1.js
@@ -1,28 +1,34 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
+function getTotalOutstanding() {
   const result = customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
+}
+
+function sendBill() {
   sendBill();
   return result;
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant(people) {
   for (const p of people) {
-    if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
+    if (p === "Don") {
+      return "Don";
     }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
+    if (p === "John") {
+      return "John";
     }
   }
-  return '';
+  return "";
 }
 
 function setOffAlarms(alarm, p) {
-  alarm.setOff('Found Miscreant ' + p);
+  alarm.setOff("Found Miscreant " + p);
 }
