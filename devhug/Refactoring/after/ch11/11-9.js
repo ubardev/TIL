7ca959e7b8fe25ee2981ea4f1f4137f370a6627a
Ubar <bellin83@gmail.com>
2022-07-06 07:19:57 +0900

    :sparkles: 11.10 함수를 명령으로 바꾸기(책 11.9)

--- devhug/Refactoring/after/ch11/11-9.js
+++ devhug/Refactoring/after/ch11/11-9.js
@@ -1,20 +1,34 @@
-export function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+class Score {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this.candidate = candidate;
+    this.medicalExam = medicalExam;
+    this.scoringGuide = scoringGuide;
+  }
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+  execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this.medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = "regular";
+    if (
+      this.scoringGuide.stateWithLowCertification(this.candidate.originState)
+    ) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
-  }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
+}
+
+export function score(candidate, medicalExam, scoringGuide) {
+  return new Score(candidate, medicalExam, scoringGuide).execute();
 }
 
 export class ScoringGuide {
