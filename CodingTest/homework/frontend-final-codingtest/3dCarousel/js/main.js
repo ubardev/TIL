3f266da1e971c3b6d5f9bfaa09c22686aaeea294
Ubar <bellin83@gmail.com>
2022-07-12 06:21:43 +0900

    :sparkles: 스크립트 - 마무리

--- CodingTest/homework/frontend-final-codingtest/3dCarousel/js/main.js
+++ CodingTest/homework/frontend-final-codingtest/3dCarousel/js/main.js
@@ -4,6 +4,46 @@
   const prevButton = document.querySelector(".prev-btn");
   const nextButton = document.querySelector(".next-btn");
 
+  function changeTransform() {
+    const items = document.querySelectorAll(".carousel-item");
+
+    items.forEach((e, i) => {
+      let degree = 360 / items.length;
+
+      if (items.length > 1) {
+        if (i === 0) {
+          e.style.transform = "rotateY(0deg) translateZ(250px)";
+        } else {
+          e.style.transform = `rotateY(${
+            degree * i
+          }deg) translateZ(250px) rotateY(-${degree * i}deg)`;
+        }
+      }
+
+      if (items.length >= 5) {
+        if (i === 0) {
+          e.style.transform = "rotateY(0deg) translateZ(250px)";
+        } else if (i === 1) {
+          e.style.transform =
+            "rotateY(72deg) translateZ(250px) rotateY(-72deg)";
+        } else if (i === 2) {
+          e.style.transform =
+            "rotateY(144deg) translateZ(250px) rotateY(-144deg) translateX(400px)";
+        } else if (i === items.length - 2) {
+          e.style.transform =
+            "rotateY(216deg) translateZ(250px) rotateY(-216deg) translateX(-400px)";
+        } else if (i === items.length - 1) {
+          e.style.transform =
+            "rotateY(288deg) translateZ(250px) rotateY(-288deg)";
+        } else {
+          e.style.transform = `rotateY(${
+            degree * i
+          }deg) translateZ(250px) rotateY(-${degree * i}deg)`;
+        }
+      }
+    });
+  }
+
   function moveNext() {
     const items = document.querySelectorAll(".carousel-item");
 
@@ -14,6 +54,7 @@
       currentItem.classList.remove("now");
       next.classList.add("now");
     }
+    changeTransform();
   }
 
   function movePrev() {
@@ -27,6 +68,7 @@
       currentItem.classList.remove("now");
       lastItem.classList.add("now");
     }
+    changeTransform();
   }
 
   function createTag(url) {
@@ -54,6 +96,7 @@
       reader.onload = (e) => {
         const imgUrl = e.target.result;
         carouselUl.insertBefore(createTag(imgUrl), items[0]);
+        changeTransform();
       };
 
       reader.readAsDataURL(value.files[0]);
@@ -65,4 +108,8 @@
   });
   nextButton.addEventListener("click", moveNext);
   prevButton.addEventListener("click", movePrev);
+
+  window.onload = () => {
+    changeTransform();
+  };
 })();
