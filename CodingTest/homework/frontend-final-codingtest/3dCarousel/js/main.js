5b982f099451903a3e93c2ab3218d1c14f2a15e9
Ubar <bellin83@gmail.com>
2022-07-12 06:02:22 +0900

    :sparkles: 스크립트 - 이미지 업로드 기능 구현

--- CodingTest/homework/frontend-final-codingtest/3dCarousel/js/main.js
+++ CodingTest/homework/frontend-final-codingtest/3dCarousel/js/main.js
@@ -15,8 +15,54 @@
       next.classList.add("now");
     }
   }
-  function movePrev() {}
 
+  function movePrev() {
+    const items = document.querySelectorAll(".carousel-item");
+
+    if (items.length > 1) {
+      const currentItem = document.querySelector(".now");
+      const lastItem = carouselUl.lastElementChild;
+
+      carouselUl.insertBefore(lastItem, items[0]);
+      currentItem.classList.remove("now");
+      lastItem.classList.add("now");
+    }
+  }
+
+  function createTag(url) {
+    const list = document.createElement("li");
+    const img = document.createElement("img");
+    list.classList.add("carousel-item");
+    img.src = url;
+    list.appendChild(img);
+
+    const items = document.querySelectorAll(".carousel-item");
+    items.forEach((item) => {
+      item.classList.remove("now");
+    });
+    list.classList.add("now");
+
+    return list;
+  }
+
+  function uploadImage(value) {
+    const items = document.querySelectorAll(".carousel-item");
+
+    if (value.files) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const imgUrl = e.target.result;
+        carouselUl.insertBefore(createTag(imgUrl), items[0]);
+      };
+
+      reader.readAsDataURL(value.files[0]);
+    }
+  }
+
+  imageInput.addEventListener("change", (e) => {
+    uploadImage(e.target);
+  });
   nextButton.addEventListener("click", moveNext);
   prevButton.addEventListener("click", movePrev);
 })();
