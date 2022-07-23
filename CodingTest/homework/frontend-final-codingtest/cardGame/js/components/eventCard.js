fd22b6ce0eb3ccde1afa885d31496fb10d361a03
Ubar <bellin83@gmail.com>
2022-07-24 06:56:45 +0900

    :sparkles: 스크립트 - 카드 판별 로직

--- CodingTest/homework/frontend-final-codingtest/cardGame/js/components/eventCard.js
+++ CodingTest/homework/frontend-final-codingtest/cardGame/js/components/eventCard.js
@@ -1,12 +1,40 @@
 class EventCard {
-  constructor() {}
+  constructor() {
+    this.cards = document.querySelector(".list-card");
+    this.cardEl = [];
+  }
 
   setup() {
     this.bindEvents();
   }
 
   bindEvents() {
-    console.log("hello~");
+    this.cards.addEventListener("click", (event) => {
+      const elClicked = event.target;
+      if ((elClicked.targetName = "LI")) {
+        if (this.cardEl.length < 2 && this.cardEl[0] !== elClicked) {
+          this.cardEl.push(elClicked);
+          elClicked.classList.add("on");
+
+          if (this.cardEl.length === 2) {
+            setTimeout(() => {
+              this.cardEl.forEach((item) => {
+                // 같은 카드를 선택했을 경우
+                if (
+                  this.cardEl[0].dataset.name === this.cardEl[1].dataset.name
+                ) {
+                  item.style.visibility = "hidden";
+                } else {
+                  // 다른 카드를 선택했을 경우
+                  item.classList.remove("on");
+                }
+              });
+              this.cardEl.splice(0, 2);
+            }, 500);
+          }
+        }
+      }
+    });
   }
 }
 
