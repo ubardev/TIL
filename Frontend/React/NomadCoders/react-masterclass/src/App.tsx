446231b5fe6c92d9e59eb7da4499cd5bd0077277
ubardev <bellin83@gmail.com>
2022-08-07 06:36:49 +0900

    :sparkles: 2.7 Themes

--- Frontend/React/NomadCoders/react-masterclass/src/App.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/App.tsx
@@ -1,11 +1,15 @@
 import styled, { keyframes } from 'styled-components';
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 const rotationAnimation = keyframes`
   0% {
