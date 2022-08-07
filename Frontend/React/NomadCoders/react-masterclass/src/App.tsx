f0b2969974cbd2edc491e4baf980165449011bed
ubardev <bellin83@gmail.com>
2022-08-08 06:18:12 +0900

    :sparkles: 3.2 Typing the Props

--- Frontend/React/NomadCoders/react-masterclass/src/App.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/App.tsx
@@ -1,54 +1,11 @@
-import styled, { keyframes } from 'styled-components';
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
-`;
+import Circle from './Circle';
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Emoji>😂</Emoji>
-      </Box>
-      <Emoji>🎇</Emoji>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
