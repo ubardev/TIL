a074abb513a35d49b5be344db6fe393900a4ee4b
ubardev <bellin83@gmail.com>
2022-08-10 05:18:37 +0900

    :sparkles: 4.0 Setup

--- Frontend/React/NomadCoders/react-masterclass/src/App.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Router from './Router';
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.bgColor};
@@ -9,11 +10,7 @@ const H1 = styled.h1`
 `;
 
 function App() {
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
