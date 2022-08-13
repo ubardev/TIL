a56d0a740364954f8322e496f010c0398c2345cf
ubardev <bellin83@gmail.com>
2022-08-14 04:34:44 +0900

    :sparkles: 4.3 변수 이름 짓는 법 (나쁜 예제 💩)

---
+++ Frontend/Javascript/DreamCoding/2.naming.js
@@ -0,0 +1,33 @@
+// 주석을 달명 됨
+
+/** 변수 규칙!
+ * 라틴문자(0-9, a-z, A-Z), _
+ * 대소문자를 구분함
+ * 추천: camelCase (likeThis)
+ * 한국어 X
+ * 예약어 X
+ * 숫자로 시작 X
+ * 특수문자 X (_, $ 두가지는 예외)
+ * 이모지 X
+ */
+
+let apple;
+let redApple;
+
+// 나쁜예제
+let num = 20;
+
+// 좋은 예제
+let myAge = 20;
+
+// 나쁜예제
+let audio1;
+let audio2;
+
+// 좋은 예제
+let backgroundAudio;
+let windAudio;
+
+// 꿀팁!
+let audioBackground;
+let audioWind;
