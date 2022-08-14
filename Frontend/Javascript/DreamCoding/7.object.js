b6c2a169c3b7e73d0025ba5ccc38b910e3eb8a6c
ubardev <bellin83@gmail.com>
2022-08-15 05:16:19 +0900

    :sparkles: 4.7 Nested Routes part One

---
+++ Frontend/Javascript/DreamCoding/7.object.js
@@ -0,0 +1,29 @@
+let name = 'apple';
+let color = 'red';
+let display = 'redApple';
+let orangeName = 'orange';
+
+let apple = {
+  name: 'apple',
+  color: 'red',
+  display: 'redApple',
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+let orange = {
+  name: 'orange',
+  color: 'orange',
+  display: 'orangeOrange',
+};
+
+// code
+// data
+// stack
+// heap
+
+// 원시 primitive는 Data, Stack에 들어감
+// 객체는 heap에 들어감
