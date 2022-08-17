e71b3b605e46f96b142cabe85b64335a391f6185
ubardev <bellin83@gmail.com>
2022-08-17 17:05:17 +0900

    :sparkles: 4.12 Price Chart

--- Frontend/React/NomadCoders/react-masterclass/src/api.ts
+++ Frontend/React/NomadCoders/react-masterclass/src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const NICO_BASE_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
@@ -10,4 +11,12 @@ export function fetchCoinInfo(coinId: string) {
 
 export function fetchCoinTickers(coinId: string) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7;
+  return fetch(`${NICO_BASE_URL}?coinId=${coinId}&start=${startDate}&end=${endDate}`).then(
+    (response) => response.json(),
+  );
 }
--- Frontend/React/NomadCoders/react-masterclass/src/routes/Coin.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/routes/Coin.tsx
@@ -221,7 +221,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
