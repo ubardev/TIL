ec6e4330f8064e727388f9c7614e989d0ada32ac
ubardev <bellin83@gmail.com>
2022-09-06 20:46:47 +0900

    :sparkles: 5.18 Enums

--- Frontend/React/NomadCoders/react-masterclass/src/components/ToDoList.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/components/ToDoList.tsx
@@ -7,7 +7,7 @@ function ToDoList() {
   const toDos = useRecoilValue(toDoSelector);
   const [category, setCategory] = useRecoilState(categoryState);
   const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    setCategory(event.currentTarget.value);
+    setCategory(event.currentTarget.value as any);
   };
 
   return (
