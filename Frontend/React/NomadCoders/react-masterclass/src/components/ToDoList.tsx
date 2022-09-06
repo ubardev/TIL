0efcaaa632622be82e9aaf54c45743f6414ecbef
ubardev <bellin83@gmail.com>
2022-09-07 05:15:24 +0900

    :sparkles: 5.18 Enums

--- Frontend/React/NomadCoders/react-masterclass/src/components/ToDoList.tsx
+++ Frontend/React/NomadCoders/react-masterclass/src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { categoryState, toDoSelector } from '../atoms';
+import { Categories, categoryState, toDoSelector } from '../atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
@@ -15,9 +15,9 @@ function ToDoList() {
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
+        <option value={Categories.TO_DO}>To Do</option>
+        <option value={Categories.DOING}>Doing</option>
+        <option value={Categories.DONE}>Done</option>
       </select>
       <CreateToDo />
       {toDos?.map((toDo) => (
