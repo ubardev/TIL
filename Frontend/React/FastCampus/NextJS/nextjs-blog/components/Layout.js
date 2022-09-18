dc70103295eb54139e0d6375c6be4e0ca9563314
ubardev <bellin83@gmail.com>
2022-09-18 23:16:33 +0900

    :sparkles: 라우팅

--- Frontend/React/FastCampus/NextJS/nextjs-blog/components/Layout.js
+++ Frontend/React/FastCampus/NextJS/nextjs-blog/components/Layout.js
@@ -20,8 +20,19 @@ export default function Layout({ children }) {
           Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
 
-      <style jsx>{`
+        * {
+          box-sizing: border-box;
+        }
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -149,21 +160,6 @@ export default function Layout({ children }) {
             width: 100%;
             flex-direction: column;
           }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
         }
       `}</style>
     </div>
