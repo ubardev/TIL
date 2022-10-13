172bf1cb55a0e5e3e8fff4570de3d6947d256533
ubardev <bellin83@gmail.com>
2022-10-14 05:39:42 +0900

    :sparkles: add scope

---
+++ Frontend/Javascript/DreamCoding/14.scope/2.scope-quiz.js
@@ -0,0 +1,18 @@
+{
+  const x = 1;
+  {
+    const y = 2;
+    console.log(x);
+  }
+  console.log(x);
+  // console.log(y);
+}
+
+const text = 'global'; // 전역 변수, 전역 스코프 (글로벌 변수, 글로벌 스코프)
+{
+  const text = 'inside block1'; // 지역 변수(로컬변수), 지역 스코프(로컬스코프)
+  {
+    const text = 'inside block2';
+    console.log(text);
+  }
+}
