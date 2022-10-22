4c82765f5d29e121ac71a75515c3638cb8b2b528
ubardev <bellin83@gmail.com>
2022-10-22 11:56:59 +0900

    :sparkles: 01. 프로젝트 시작(Link Component - Client-Side Naivgation)

--- Frontend/React/FastCampus/NextJS/blog/pages/index.js
+++ Frontend/React/FastCampus/NextJS/blog/pages/index.js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,6 +10,17 @@ export default function Home() {
       </Head>
 
       <main>
+        <h1 className="title">
+          Read this Post{' '}
+          <Link href="/posts/first-post">
+            <a>첫번째 글</a>
+          </Link>
+        </h1>
+
+        <h1 className="title">
+          Read this Post <a href="/posts/first-post">첫번째 글(a tag)</a>
+        </h1>
+
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -48,14 +60,17 @@ export default function Home() {
         </div>
       </main>
 
+      <Link href="/posts/second-post">
+        <a>두번째 글</a>
+      </Link>
+
       <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +220,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
--- Job/Interview/React/NextJSRouting.md
+++ Job/Interview/React/NextJSRouting.md
@@ -1,3 +1,22 @@
 - location.replace("url"): 로컬 state 유지 안됨(리렌더)
 - router.push(url): 로컬 state유지 / data fetching은 일어남
 - router.push(url, as, {shallow: true}): 로컬 state유지 / data fetching x
+
+---
+
+- <a href="/posts/first-post">첫번째 글(a tag)</a>
+  - 모든걸 새로 불러옴
+  - reload시킴
+- <Link href="/posts/first-post"><a>첫번째 글</a></Link>
+  - 필요한 부분들만 가져옴
+
+Client Side Navigate
+browser에서 url을 직접 쳐서 이동하는 것과 달리 JS상에서 page컴포넌트를 교체하는 것
+
+Cide Splitting
+Next.js는 Automatic Code Splitting을 제공
+
+- 특정 페이지에 접근 할 때는 해당 페이지를 그릴 때 필요한 chunk만 로드
+- 페이지 이동을 할 땐 목적지 페이지에 필요한 chunk만 추가 로드
+
+이를 통해 성능이 최적화 된다.
