9ee815b17d1dd0b8a81c4ac542672bd59df2c324
ubardev <bellin83@gmail.com>
2022-11-13 07:15:34 +0900

    :sparkles: JSX 문법 정리리

--- Frontend/React/DreamCoding/basic/src/App.js
+++ Frontend/React/DreamCoding/basic/src/App.js
@@ -2,23 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '엘리';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="orange">Hello!</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        <li>우유</li>
+        <li>딸기</li>
+        <li>바나나</li>
+      </ul>
+      <img
+        style={{ width: '200px', height: '200px' }}
+        src="https://images.unsplash.com/photo-1668241282073-2cf47bae10d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60"
+        alt="nature"
+      />
+    </>
   );
 }
 
