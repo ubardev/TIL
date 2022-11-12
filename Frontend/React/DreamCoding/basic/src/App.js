9a5874a6819d19e379a61494be4f46649cb72400
ubardev <bellin83@gmail.com>
2022-11-13 07:21:58 +0900

    :sparkles: JSX 문법 정리 (심화)

--- Frontend/React/DreamCoding/basic/src/App.js
+++ Frontend/React/DreamCoding/basic/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 
 function App() {
   const name = '엘리';
+  const list = ['우유', '딸기', '바나나', '요거트'];
+
   return (
     <>
-      <h1 className="orange">Hello!</h1>
+      <h1 className="orange">{`Hello! ${name}`}</h1>
       <h2>Hello!</h2>
       <p>{name}</p>
       <ul>
-        <li>우유</li>
-        <li>딸기</li>
-        <li>바나나</li>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
       </ul>
       <img
         style={{ width: '200px', height: '200px' }}
