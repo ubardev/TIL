aa07db87a42f8cc111fe1a8a034d48c52b853dbb
ubardev <bellin83@gmail.com>
2022-11-14 05:30:25 +0900

    :sparkles: 컴포넌트 만드는 꿀팁

--- Frontend/React/DreamCoding/basic/src/AppJSX.jsx
+++ Frontend/React/DreamCoding/basic/src/AppJSX.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function AppJSX() {
   const name = '엘리';
   const list = ['우유', '딸기', '바나나', '요거트'];
 
@@ -24,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppJSX;
