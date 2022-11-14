972c5e25d62f461b8cef2327a62a5051bb29bd46
ubardev <bellin83@gmail.com>
2022-11-15 06:24:45 +0900

    :sparkles: Profile 첫 컴포넌트 만들기

--- Frontend/React/DreamCoding/basic/src/components/Profile.jsx
+++ Frontend/React/DreamCoding/basic/src/components/Profile.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Profile() {
-  return <div></div>;
+  return (
+    <div className="profile">
+      <img
+        src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+        alt="avatar"
+        className="photo"
+      />
+      <h1>James Kim</h1>
+      <p>프론트엔드 개발자</p>
+    </div>
+  );
 }
