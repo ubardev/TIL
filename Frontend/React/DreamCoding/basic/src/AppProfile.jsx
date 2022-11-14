6ffa939d0d9ab73577e9a871dd998f505ff56eab
ubardev <bellin83@gmail.com>
2022-11-15 07:17:12 +0900

    :sparkles: Event 처리하기

--- Frontend/React/DreamCoding/basic/src/AppProfile.jsx
+++ Frontend/React/DreamCoding/basic/src/AppProfile.jsx
@@ -3,8 +3,13 @@ import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (event) => {
+    console.log(event);
+    alert('버튼 클릭!');
+  };
   return (
     <>
+      <button onClick={handleClick}>버튼</button>
       <Avatar
         image="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
         isNew={true}
