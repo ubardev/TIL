34afea76665779af0858a85b192dfce1d986659f
ubardev <bellin83@gmail.com>
2022-11-15 07:08:29 +0900

    :sparkles: 아바타 컴포넌트 만들기

--- Frontend/React/DreamCoding/basic/src/AppProfile.jsx
+++ Frontend/React/DreamCoding/basic/src/AppProfile.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Avatar from './components/Avatar';
 import Profile from './components/Profile';
 
 function AppProfile() {
   return (
     <>
+      <Avatar
+        image="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+        isNew={true}
+      />
       <Profile
         image="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
         name="James Kim"
--- Frontend/React/DreamCoding/basic/src/components/Profile.jsx
+++ Frontend/React/DreamCoding/basic/src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import Avatar from './Avatar';
 
 export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
-      <img className="photo" src={image} alt="avatar" />
-      {isNew && <span className="new">New</span>}
+      <Avatar image={image} isNew={isNew} />
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
