74320fae5882988c436841ff8fcc453542f4da0d
ubardev <bellin83@gmail.com>
2022-11-15 06:52:46 +0900

    :sparkles: 새로운 개발자 태그 만들기

--- Frontend/React/DreamCoding/basic/src/components/Profile.jsx
+++ Frontend/React/DreamCoding/basic/src/components/Profile.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Profile(props) {
+export default function Profile({ image, name, title, isNew }) {
   return (
     <div className="profile">
-      <img className="photo" src={props.image} alt="avatar" />
-      <h1>{props.name}</h1>
-      <p>{props.title}</p>
+      <img className="photo" src={image} alt="avatar" />
+      {isNew && <span className="new">New</span>}
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
