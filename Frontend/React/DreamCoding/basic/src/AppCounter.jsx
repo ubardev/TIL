2ec0ff29a7df70f0a6680cc479e3e837d6c032bc
ubardev <bellin83@gmail.com>
2022-11-16 07:19:28 +0900

    :sparkles: 카운터 상태 끌어 올리기 solution

--- Frontend/React/DreamCoding/basic/src/AppCounter.jsx
+++ Frontend/React/DreamCoding/basic/src/AppCounter.jsx
@@ -4,14 +4,17 @@ import Counter from './components/Counter';
 
 export default function AppCounter() {
   const [count, setCount] = useState(0);
+  //   const handleClick = setCount((prev) => prev + 1);
+  const handleClick = () => setCount((prev) => prev + 1);
+
   return (
     <div className="container">
       <div className="banner">
         Total Count: {count} {count > 10 ? '🔥' : '❄️'}
       </div>
       <div className="counters">
-        <Counter />
-        <Counter />
+        <Counter total={count} onClick={handleClick} />
+        <Counter total={count} onClick={handleClick} />
       </div>
     </div>
   );
