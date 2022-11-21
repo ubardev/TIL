ba4682bcb4932d15279257096056590d30ab3fbb
ubardev <bellin83@gmail.com>
2022-11-22 05:44:32 +0900

    :sparkles: 5.3 마우스 따라 가기 - 기본 구현💡

--- Frontend/React/DreamCoding/basic/src/AppXY.jsx
+++ Frontend/React/DreamCoding/basic/src/AppXY.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
   return (
-    <div className='container'>
-      <div className='pointer' />
+    <div
+      className="container"
+      onMouseMove={(e) => {
+        setX(e.clientX);
+        setY(e.clientY);
+      }}
+    >
+      <div
+        className="pointer"
+        style={{ transform: `translate(${x}px, ${y}px)` }}
+      />
     </div>
   );
 }
