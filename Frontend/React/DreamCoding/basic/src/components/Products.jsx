3d6c48a247e27eade7d2a47d442c796283404d18
ubardev <bellin83@gmail.com>
2022-11-22 05:18:29 +0900

    :sparkles: 4.20 useEffect 제대로 사용하기

--- Frontend/React/DreamCoding/basic/src/components/Products.jsx
+++ Frontend/React/DreamCoding/basic/src/components/Products.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
-  const [count, setCount] = useState(0);
+  const [checked, setChecked] = useState(false);
   const [products, setProducts] = useState([]);
+  const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
-    fetch('data/products.json')
+    fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
@@ -14,10 +15,12 @@ export default function Products() {
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input type="checkbox" value={checked} onChange={handleChange} />
+      <lable htmlFor="checkbox">Show Only hot sale</lable>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
@@ -28,7 +31,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount((prev) => prev + 1)}>{count}</button>
     </>
   );
 }
