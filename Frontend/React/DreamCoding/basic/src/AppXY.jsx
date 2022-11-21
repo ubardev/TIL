00a43c34c965bbeda90af9d6a38ac5edd7550b48
ubardev <bellin83@gmail.com>
2022-11-22 05:51:01 +0900

    :sparkles: 5.4 마우스 따라 가기 - 개선 하기💡

--- Frontend/React/DreamCoding/basic/src/AppXY.jsx
+++ Frontend/React/DreamCoding/basic/src/AppXY.jsx
@@ -2,20 +2,20 @@ import React, { useState } from 'react';
 import './AppXY.css';
 
 export default function AppXY() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0, z: 0 });
 
   return (
     <div
       className="container"
       onMouseMove={(e) => {
-        setX(e.clientX);
-        setY(e.clientY);
+        // setPosition({ x: e.clientX, y: e.clientY });
+        // 만약 수평으로만 이동이 가능하다면?
+        setPosition((prev) => ({ ...prev, x: e.clientX }));
       }}
     >
       <div
         className="pointer"
-        style={{ transform: `translate(${x}px, ${y}px)` }}
+        style={{ transform: `translate(${position.x}px, ${position.y}px` }}
       />
     </div>
   );
