b94bed58086343780cd09b667611647bcd548031
ubardev <bellin83@gmail.com>
2022-11-30 05:32:08 +0900

    :sparkles: 5.22 로딩, 에러 상태 추가 💡

--- Frontend/React/DreamCoding/basic/src/basic/components/Products.jsx
+++ Frontend/React/DreamCoding/basic/src/basic/components/Products.jsx
@@ -1,31 +1,40 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
+    setLoading(true);
+    setError();
+
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
         setProducts(data);
-      });
+      })
+      .catch((e) => setError('에러가 발생했음!'))
+      .finally(() => setLoading(false));
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
   }, [checked]);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <>
       <input
-        id='checkbox'
-        type='checkbox'
+        id="checkbox"
+        type="checkbox"
         value={checked}
         onChange={handleChange}
       />
-      <label htmlFor='checkbox'>Show Only 🔥 Sale</label>
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
