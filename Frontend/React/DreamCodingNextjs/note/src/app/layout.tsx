4c89091c53c7faa08b508eed28d206c827a7590a
Austin-myfranchise <austin@myfranchise.kr>
2023-05-07 01:05:03 +0900

    4.12 레이아웃으로 골격을!

--- Frontend/React/DreamCodingNextjs/note/src/app/layout.tsx
+++ Frontend/React/DreamCodingNextjs/note/src/app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
+import "./globals.css";
+import styles from "./layout.module.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,16 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header className={styles.header}>
+          <h1>Demo Note</h1>
+          <nav className={styles.nav}>
+            <a href="">Contact</a>
+            <a href="">About</a>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- Frontend/React/DreamCodingNextjs/note/src/app/page.tsx
+++ Frontend/React/DreamCodingNextjs/note/src/app/page.tsx
@@ -4,6 +4,5 @@ import { notFound } from "next/navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  notFound();
   return <h1>홈페이지다!!</h1>;
 }
