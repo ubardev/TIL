031395ae85f3b7dfcf17c6d15eb13b75b086d6a6
Ubar <bellin83@gmail.com>
2023-05-07 01:26:52 +0900

    4.17 제품 링크 만들기💡

--- Frontend/React/DreamCodingNextjs/note/src/app/products/page.tsx
+++ Frontend/React/DreamCodingNextjs/note/src/app/products/page.tsx
@@ -1,22 +1,19 @@
 import Link from "next/link";
 
+const products = ["skirt", "pants", "skirt", "shoes"];
+
 export default function ProductsPage() {
   return (
     <>
       <h1>제품 소개 페이지</h1>
       <ul>
-        <li>
-          <Link href="/products/shirt">shirt</Link>
-        </li>
-        <li>
-          <Link href="/products/pants">pants</Link>
-        </li>
-        <li>
-          <Link href="/products/skirt">skirt</Link>
-        </li>
-        <li>
-          <Link href="/products/shoes">shoes</Link>
-        </li>
+        {products.map((product, index) => {
+          return (
+            <li key={index}>
+              <Link href={`/products/${product}`}>{product}</Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
