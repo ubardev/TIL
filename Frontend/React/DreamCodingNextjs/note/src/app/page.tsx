7f0d7b58e3a29cba1e71860496efbe8ee6fc3999
Ubar <bellin83@gmail.com>
2023-05-08 01:32:18 +0900

    5.3 서버 컴포넌트

--- Frontend/React/DreamCodingNextjs/note/src/app/page.tsx
+++ Frontend/React/DreamCodingNextjs/note/src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Inter } from "@next/font/google";
 import { notFound } from "next/navigation";
+import os from "os"; // 노드 APIs
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  console.log("안녕!");
+  console.log("os.hostname() ==========>", os.hostname());
   return <h1>홈페이지다!!</h1>;
 }
