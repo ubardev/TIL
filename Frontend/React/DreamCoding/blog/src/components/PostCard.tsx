f00a59a25d8332b9abf020418fb0e2329826cf86
Ubar <bellin83@gmail.com>
2023-09-24 02:27:18 +0900

    :lipstick: Post Card 스타일링

--- Frontend/React/DreamCoding/blog/src/components/PostCard.tsx
+++ Frontend/React/DreamCoding/blog/src/components/PostCard.tsx
@@ -11,18 +11,23 @@ export default function PostCard({ post }: IProps) {
 
   return (
     <Link href={`/posts/${path}`}>
-      <Image
-        src={`/images/posts/${path}.png`}
-        alt={title}
-        width={300}
-        height={200}
-      />
-      <div>
-        <time>{date.toString()}</time>
-        <h3>{title}</h3>
-        <h3>{description}</h3>
-        <h3>{category}</h3>
-      </div>
+      <article className="rounded-md overflow-hidden shadow-lg">
+        <Image
+          className="w-full"
+          src={`/images/posts/${path}.png`}
+          alt={title}
+          width={300}
+          height={200}
+        />
+        <div className="flex flex-col items-center p-4">
+          <time className="self-end">{date.toString()}</time>
+          <h3 className="text-lg font-bold">{title}</h3>
+          <p className="w-full truncate text-center">{description}</p>
+          <span className="text-sm rounded-lg bg-green-100 px-2 my-2">
+            {category}
+          </span>
+        </div>
+      </article>
     </Link>
   );
 }
--- Frontend/React/DreamCoding/blog/src/components/PostsGrid.tsx
+++ Frontend/React/DreamCoding/blog/src/components/PostsGrid.tsx
@@ -7,7 +7,7 @@ interface IProps {
 
 export default function PostsGrid({ posts }: IProps) {
   return (
-    <ul>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post) => (
         <li key={post.path}>
           <PostCard post={post} />
