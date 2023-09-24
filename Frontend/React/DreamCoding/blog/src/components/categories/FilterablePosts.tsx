ae7264d7cfae32643eacae74c8c1371c6eb89c86
Ubar <bellin83@gmail.com>
2023-09-25 00:10:56 +0900

    :necktie: 필터가능한 포스트 페이지 추가가

---
+++ Frontend/React/DreamCoding/blog/src/components/categories/FilterablePosts.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+import { Post } from "@/service/posts";
+import PostsGrid from "../PostsGrid";
+import Categories from "./Categories";
+
+interface IProps {
+  posts: Post[];
+  categories: string[];
+}
+
+const ALL_POSTS = "All Posts";
+
+export default function FilterablePosts({ posts, categories }: IProps) {
+  const [selected, setSelected] = useState(ALL_POSTS);
+  const filtered =
+    selected === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category === selected);
+
+  return (
+    <section>
+      <PostsGrid posts={filtered} />
+      <Categories
+        categories={[ALL_POSTS, ...categories]}
+        selected={selected}
+        onClick={setSelected}
+      />
+    </section>
+  );
+}
