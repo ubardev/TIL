0a27a113eed138bc626d6835f019b9e9d03d204f
Ubar <bellin83@gmail.com>
2023-09-25 02:10:50 +0900

    :necktie: 마크다운 뷰어 상세 정보 추가

--- Frontend/React/DreamCoding/blog/src/app/posts/[slug]/page.tsx
+++ Frontend/React/DreamCoding/blog/src/app/posts/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { AiTwotoneCalendar } from "react-icons/ai";
 import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostDate } from "@/service/posts";
 
@@ -8,12 +10,27 @@ interface IProps {
 }
 
 export default async function PostPage({ params: { slug } }: IProps) {
-  const post = await getPostDate(slug);
+  const { title, description, date, path, content } = await getPostDate(slug);
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      <MarkdownViewer content={post.content} />
-    </>
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+      <Image
+        className="w-full h-1/5 max-h-[500px]"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-green-600">
+          <AiTwotoneCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-44 border-2 border-green-600 mt-4 mb-8" />
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
