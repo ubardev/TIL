4f747e178d0c20ea66aab87814da3b55c7687431
Ubar <bellin83@gmail.com>
2023-09-25 14:32:35 +0900

    :necktie: 마크다운 코드, 이미지 영역 셋팅

--- Frontend/React/DreamCoding/blog/src/components/MarkdownViewer.tsx
+++ Frontend/React/DreamCoding/blog/src/components/MarkdownViewer.tsx
@@ -1,10 +1,48 @@
+"use client";
+
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
-      {content}
-    </ReactMarkdown>
+    <div className="w-full flex justify-center">
+      <ReactMarkdown
+        className="prose max-w-3xl"
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                language={match[1]}
+                PreTag="div"
+                {...props}
+                style={materialDark}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+          img: (image) => (
+            <Image
+              className="w-full max-h-60 object-cover"
+              src={image.src || ""}
+              alt={image.alt || ""}
+              width={500}
+              height={350}
+            />
+          ),
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
   );
 }
