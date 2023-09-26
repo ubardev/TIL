b9f4dcae4ccda5cabe4fd958157709f1c4dac895
Ubar <bellin83@gmail.com>
2023-09-27 08:57:34 +0900

    :necktie: 정보, 연락 페이지 추가

--- Frontend/React/DreamCoding/blog/src/app/about/page.tsx
+++ Frontend/React/DreamCoding/blog/src/app/about/page.tsx
@@ -1,3 +1,28 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = "text-2xl font-bold text-gray-800 my-2";
+
 export default function AboutPage() {
-  return <p>나에 대해!</p>;
+  return (
+    <>
+      <Hero />
+      <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+        <h2 className={TITLE_CLASS}>Who Am I?</h2>
+        <p>
+          개발을 좋아하는 프론트엔드 개발자입니다.
+          <br />
+          현재 마이프랜차이즈에서 프론트엔드 개발을 하고 있습니다.
+        </p>
+        <h2 className={TITLE_CLASS}>Career</h2>
+        <p>
+          마이프랜차이즈 (2023.01 ~ 현재) <br />
+          토스랩 (2018.08 ~ 2022.12) <br />
+          에스티유니타스 ( 2015.06 ~ 2018.04) <br />
+          쌍용정보통신 (2011.06 ~ 2015.06)
+        </p>
+        <h2 className={TITLE_CLASS}>Skills</h2>
+        <p>React, Next.js</p>
+      </section>
+    </>
+  );
 }
--- Frontend/React/DreamCoding/blog/src/app/contact/page.tsx
+++ Frontend/React/DreamCoding/blog/src/app/contact/page.tsx
@@ -1,3 +1,39 @@
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { FaBlog } from "react-icons/fa";
+import ContactForm from "@/components/ContactForm";
+
+const LINKS = [
+  { icon: <AiFillGithub />, url: "https://github.com/ubardev" },
+  {
+    icon: <AiFillLinkedin />,
+    url: "https://www.linkedin.com/in/jong-in-hwang-876619157",
+  },
+  {
+    icon: <FaBlog />,
+    url: "https://freelearn.tistory.com",
+  },
+];
+
 export default function ContactPage() {
-  return <p>연락하기</p>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p>bellin83@gmail.com</p>
+      <ul className="flex gap-8 my-2">
+        {LINKS.map((link, index) => (
+          <a
+            key={index}
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            className="text-5xl hover:text-green-400"
+          >
+            {link.icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
