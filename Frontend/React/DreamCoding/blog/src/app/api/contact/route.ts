c542034f6dfe4cc89f39a01066bec5394ca9d423
Ubar <bellin83@gmail.com>
2023-09-30 03:27:00 +0900

    :necktie: 이메일 발송 서버 로직 추가

--- Frontend/React/DreamCoding/blog/src/app/api/contact/route.ts
+++ Frontend/React/DreamCoding/blog/src/app/api/contact/route.ts
@@ -1,4 +1,5 @@
 import * as yup from "yup";
+import { sendEmail } from "@/service/email";
 
 const bodySchema = yup.object().shape({
   from: yup.string().email().required(),
@@ -7,11 +8,31 @@ const bodySchema = yup.object().shape({
 });
 
 export async function POST(req: Request) {
-  if (!bodySchema.isValidSync(req.body)) {
-    return new Response("유효하지 않은 포맷입니다", { status: 400 });
+  const body = await req.json();
+
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(
+      JSON.stringify({ message: "메일 전송에 실패했습니다." }),
+      { status: 400 }
+    );
   }
 
-  const { from, subject, message } = req.body;
+  const { from, subject, message } = body;
 
   //Nodemailer를 이용해 메일 전송 로직 추가
+  return sendEmail(body) //
+    .then(
+      () =>
+        new Response(
+          JSON.stringify({ message: "메일을 성공적으로 발송했습니다." }),
+          { status: 200 }
+        )
+    ) //
+    .catch((error) => {
+      console.error(error);
+      return new Response(
+        JSON.stringify({ message: "메일 전송에 실패했습니다." }),
+        { status: 500 }
+      );
+    });
 }
