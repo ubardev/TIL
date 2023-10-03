924dd5dda58058cd8499048472faa047b0c232bd
Ubar <bellin83@gmail.com>
2023-10-04 05:27:41 +0900

    :heavy_plus_sign: Emotion 추가

--- Frontend/React/FastCampus/commerce/pages/index.tsx
+++ Frontend/React/FastCampus/commerce/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from 'next/head';
 import Image from 'next/image';
 import {useEffect, useRef, useState} from 'react';
+import {css} from '@emotion/react';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
@@ -50,8 +51,22 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <input className="" ref={inputRef} type="text" placeholder="name" />
-        <button onClick={handleClick}>Add Jacket</button>
+        <input
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          ref={inputRef}
+          type="text"
+          placeholder="name"
+        />
+        <button
+          css={css`
+            background-color: hotpink;
+            padding: 16px;
+            border-radius: 8px;
+          `}
+          onClick={handleClick}
+        >
+          Add Jacket
+        </button>
 
         <div>
           <p>Product List</p>
