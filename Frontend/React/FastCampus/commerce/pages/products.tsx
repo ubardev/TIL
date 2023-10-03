48d35226d851d3442db0733f3a1f650196607969
Ubar <bellin83@gmail.com>
2023-10-04 05:48:56 +0900

    :heavy_plus_sign: Slide 라이브러리 테스트

---
+++ Frontend/React/FastCampus/commerce/pages/products.tsx
@@ -0,0 +1,53 @@
+// import ImageGallery from 'react-image-gallery';
+
+import Image from 'next/image';
+import Carousel from 'nuka-carousel';
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1011/1000/600/",
+    thumbnail: "https://picsum.photos/id/1011/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1012/1000/600/",
+    thumbnail: "https://picsum.photos/id/1012/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1013/1000/600/",
+    thumbnail: "https://picsum.photos/id/1013/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1016/1000/600/",
+    thumbnail: "https://picsum.photos/id/1016/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
+export default function products() {
+  // return <ImageGallery items={images} />;
+  return (
+    <Carousel>
+      {images.map((item) => (
+        <Image
+          key={item.original}
+          src={item.original}
+          alt="image"
+          width={1000}
+          height={600}
+          layout="responsive"
+        />
+      ))}
+    </Carousel>
+  );
+}
