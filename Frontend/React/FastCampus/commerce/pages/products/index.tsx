6cbae59c131c217a93f6c0201ed24b2c734dc967
Ubar <bellin83@gmail.com>
2023-10-06 05:38:55 +0900

    :necktie: 상품 목록 보기 기능 추가

--- Frontend/React/FastCampus/commerce/pages/products/index.tsx
+++ Frontend/React/FastCampus/commerce/pages/products/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
 import { products } from "@prisma/client";
 
 const TAKE = 9;
@@ -13,9 +14,52 @@ export default function Products() {
       .then((data) => setProducts(data.items));
   }, []);
 
+  const getProducts = useCallback(() => {
+    const next = skip + TAKE;
+
+    fetch(`/api/get-products?skip=${next}&take=${TAKE}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const list = products.concat(data.items);
+        setProducts(list);
+      });
+    setSkip(next);
+  }, [skip, products]);
+
   return (
-    <div>
-      {products && products.map((item) => <div key={item.id}>{item.name}</div>)}
+    <div className="px-36 my-36">
+      {products && (
+        <div className="grid grid-cols-3 gap-5">
+          {products.map((item) => (
+            <div key={item.id}>
+              <Image
+                className="rounded"
+                src={item.image_url || ""}
+                alt={item.name}
+                width={300}
+                height={200}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
+              />
+              <div className="flex">
+                <span>{item.name}</span>
+                <span className="ml-auto">
+                  {item.price.toLocaleString("ko-KR")}원
+                </span>
+              </div>
+              <span className="text-zinc-400">
+                {item.category_id === 1 && "의류"}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+      <button
+        className="w-full rounded mt-20 bg-zinc-200 p-4"
+        onClick={getProducts}
+      >
+        더보기
+      </button>
     </div>
   );
 }
