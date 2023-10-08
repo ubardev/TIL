bfe184bd01a84210fc99266c11e3d66b66f9f782
Ubar <bellin83@gmail.com>
2023-10-09 03:53:13 +0900

    :necktie: Feat: 정렬 기능 추가

--- Frontend/React/FastCampus/commerce/pages/api/get-products.ts
+++ Frontend/React/FastCampus/commerce/pages/api/get-products.ts
@@ -1,9 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import { getOrderBy } from "../../constants/products";
 
 const prisma = new PrismaClient();
 
-async function getProducts(skip: number, take: number, category: number) {
+async function getProducts(
+  skip: number,
+  take: number,
+  category: number,
+  orderBy: string
+) {
   const where =
     category && category !== -1
       ? {
@@ -12,12 +18,14 @@ async function getProducts(skip: number, take: number, category: number) {
           },
         }
       : undefined;
+  const orderByCondition = getOrderBy(orderBy);
 
   try {
     const response = await prisma.products.findMany({
       skip,
       take,
       ...where,
+      ...orderByCondition,
     });
 
     return response;
@@ -35,7 +43,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { skip, take, category } = req.query;
+  const { skip, take, category, orderBy } = req.query;
 
   if (!skip || !take) {
     res.status(400).json({ message: "no skip or take" });
@@ -46,7 +54,8 @@ export default async function handler(
     const products = await getProducts(
       Number(skip),
       Number(take),
-      Number(category)
+      Number(category),
+      String(orderBy)
     );
     res.status(200).json({ items: products, message: "Success" });
   } catch (error) {
---
+++ Frontend/React/FastCampus/commerce/pages/products/page.tsx
@@ -0,0 +1,93 @@
+import { CATEGORY_MAP, FILTERS, TAKE } from "constants/products";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Input, Pagination, SegmentedControl, Select } from "@mantine/core";
+import { categories, products } from "@prisma/client";
+
+export default function Products() {
+  const [activePage, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [categories, setCategories] = useState<categories[]>([]);
+  const [selectedCategory, setCategory] = useState<string>("-1");
+  const [products, setProducts] = useState<products[]>([]);
+  const [selectedFilter, setFilter] = useState<string | null>(FILTERS[0].value);
+
+  useEffect(() => {
+    fetch("/api/get-categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data.items));
+  }, []);
+
+  useEffect(() => {
+    fetch(`/api/get-products-count?category=${selectedCategory}`)
+      .then((res) => res.json())
+      .then((data) => setTotal(Math.ceil(data.items / TAKE)));
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    const skip = TAKE * (activePage - 1);
+    fetch(
+      `/api/get-products?skip=${skip}&take=${TAKE}&category=${selectedCategory}&orderBy=${selectedFilter}`
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data.items));
+  }, [activePage, selectedCategory, selectedFilter]);
+
+  return (
+    <div className="px-36 mt-36 mb-36">
+      <div className="mb-4">
+        <Select value={selectedFilter} onChange={setFilter} data={FILTERS} />
+      </div>
+      {categories && (
+        <div className="mb-4">
+          <SegmentedControl
+            value={selectedCategory}
+            onChange={setCategory}
+            data={[
+              { label: "ALL", value: "-1" },
+              ...categories.map((category) => ({
+                label: category.name,
+                value: String(category.id),
+              })),
+            ]}
+            color="dark"
+          />
+        </div>
+      )}
+      {products && (
+        <div className="grid grid-cols-3 gap-5">
+          {products.map((item) => (
+            <div key={item.id} style={{ maxWidth: 310 }}>
+              <Image
+                className="rounded"
+                alt={item.name}
+                src={item.image_url ?? ""}
+                width={310}
+                height={390}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=="
+              />
+              <div className="flex">
+                <span>{item.name}</span>
+                <span className="ml-auto">
+                  {item.price.toLocaleString("ko-KR")}원
+                </span>
+              </div>
+              <span className="text-zinc-400">
+                {CATEGORY_MAP[item.category_id - 1]}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="w-full flex mt-5">
+        <Pagination
+          className="m-auto"
+          page={activePage}
+          onChange={setPage}
+          total={total}
+        />
+      </div>
+    </div>
+  );
+}
