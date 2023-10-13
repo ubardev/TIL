f49927d79533f619a8e7d77b602b494212317133
Ubar <bellin83@gmail.com>
2023-10-14 00:11:47 +0900

    :necktie: Feat: 매장 선택 기능 추가

--- Frontend/React/Inflearn/nextjs-map/components/home/MapSection.tsx
+++ Frontend/React/Inflearn/nextjs-map/components/home/MapSection.tsx
@@ -1,12 +1,23 @@
+import useCurrentStore from '../../hooks/useCurrentStore';
+import useMap from '../../hooks/useMap';
 import Map from './Map';
+import Markers from './Markers';
 
+import type { NaverMap } from "../../types/map";
 const MapSection = () => {
+  const { initializeMap } = useMap();
+  const { clearCurrentStore } = useCurrentStore();
+
+  const onLoadMap = (map: NaverMap) => {
+    initializeMap(map);
+    naver.maps.Event.addListener(map, "click", clearCurrentStore);
+  };
+
   return (
-    <Map
-      onLoad={() => {
-        console.log('load!');
-      }}
-    />
+    <>
+      <Map onLoad={onLoadMap} />
+      <Markers />
+    </>
   );
 };
 export default MapSection;
--- Frontend/React/Inflearn/nextjs-map/hooks/useMap.ts
+++ Frontend/React/Inflearn/nextjs-map/hooks/useMap.ts
@@ -1,4 +1,21 @@
-import {Coordinates} from '../types/store';
+import {useCallback} from 'react';
+import {mutate} from 'swr';
+
+import type { Coordinates } from "../types/store";
+import type { NaverMap } from "../types/map";
 
 export const INITIAL_CENTER: Coordinates = [37.5262411, 126.99289439];
 export const INITIAL_ZOOM = 10;
+
+export const MAP_KEY = "/map";
+
+const useMap = () => {
+  const initializeMap = useCallback((map: NaverMap) => {
+    mutate(MAP_KEY, map);
+  }, []);
+
+  return {
+    initializeMap,
+  };
+};
+export default useMap;
