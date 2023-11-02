a2d6db3494b2b13e4a59296a800d29626c544d82
Ubar <bellin83@gmail.com>
2023-11-03 02:07:46 +0900

    :necktie: 12.17 로그인 페이지 구현💡- 페이지

---
+++ Frontend/React/DreamCoding/instagram/src/app/auth/signin/page.tsx
@@ -0,0 +1,29 @@
+import {getServerSession} from 'next-auth';
+import {getProviders} from 'next-auth/react';
+import {redirect} from 'next/navigation';
+import Signin from '@/components/Signin';
+import {authOptions} from '@/pages/api/auth/[...nextauth]';
+
+type Props = {
+  searchParams: {
+    callbackUrl: string;
+  };
+};
+
+export default async function SigninPage({
+  searchParams: { callbackUrl },
+}: Props) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('/');
+  }
+
+  const providers = (await getProviders()) ?? {};
+
+  return (
+    <section className="flex justify-center mt-[30%]">
+      <Signin providers={providers} callbackUrl={callbackUrl ?? '/'} />
+    </section>
+  );
+}
