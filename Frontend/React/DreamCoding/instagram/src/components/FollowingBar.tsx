9fd61b95080122fba3233516b9941b7b94a55492
Ubar <bellin83@gmail.com>
2023-11-06 00:34:53 +0900

    :necktie: 13.6 팔로잉바💡- SWR 적용

--- Frontend/React/DreamCoding/instagram/src/components/FollowingBar.tsx
+++ Frontend/React/DreamCoding/instagram/src/components/FollowingBar.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import useSWR from 'swr';
+
 export default function FollowingBar() {
+  const { data, isLoading, error } = useSWR('/api/hello');
+  // 1. 클라이언트 콤포넌트에서 백엔드에게 api/me 사용자의 정보를 얻어옴
+  // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
+  // 3. 백엔드에서 사용자의 상세 정볼르 sanity에서 가지고 옴(followings)
+  // 4. 여기에서, 클라이언트 컴포넌트에서 followings의 정볼르 UI에 보여줌
+  //    (image, username)
+
   return <p>FollowingBar</p>;
 }
