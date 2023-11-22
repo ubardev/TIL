28fade4cabce41254ff8b5cfd09c2bdd0419e479
Ubar <bellin83@gmail.com>
2023-11-22 14:44:24 +0900

    :necktie: Feat: 검색결과 분기처리 추가

--- Frontend/React/DreamCoding/instagram/src/components/UserSearch.tsx
+++ Frontend/React/DreamCoding/instagram/src/components/UserSearch.tsx
@@ -1,14 +1,44 @@
 'use client';
 
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import useSWR from 'swr';
+import { SearchUser } from '@/model/user';
+import GridSpinner from './ui/GridSpinner';
 
 export default function UserSearch() {
-  // /api/search/${keyword}
-  // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나, 네임
-  // 검색하는 keyword가 없다면 /api/search -> 전체유저
   const [keyword, setKeyword] = useState('');
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log('data ==========>', data);
-  return <></>;
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<SearchUser[]>(`/api/search/${keyword}`);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+      {error && <p>무언가가 잘못 되었음</p>}
+      {isLoading && <GridSpinner />}
+      {!isLoading && !error && users?.length === 0 && <p>찾는 사용자가 없음</p>}
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
