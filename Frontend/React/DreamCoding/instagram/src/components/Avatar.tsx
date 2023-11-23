f396db65311833a1d88ff8db72c106ed346c34d6
Ubar <bellin83@gmail.com>
2023-11-24 06:07:58 +0900

    :lipstick: Feat: 사용자 프로필 스타일링 추가

--- Frontend/React/DreamCoding/instagram/src/components/Avatar.tsx
+++ Frontend/React/DreamCoding/instagram/src/components/Avatar.tsx
@@ -1,4 +1,4 @@
-type AvatarSize = 'small' | 'medium' | 'large';
+type AvatarSize = 'small' | 'medium' | 'large' | 'xlarge';
 
 type Props = {
   image?: string | null;
@@ -15,9 +15,8 @@ export default function Avatar({
     <div className={getContainerStyle(size, highlignt)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        className={`bg-white object-cover rounded-full p-[0.1rem] ${getImageSizeStyle(
-          size,
-        )}`}
+        className={`bg-white object-cover rounded-full 
+        ${getImageSizeStyle(size).image}`}
         alt="user profile"
         src={image ?? undefined}
         referrerPolicy="no-referrer"
@@ -31,29 +30,38 @@ function getContainerStyle(size: AvatarSize, highlight: boolean): string {
   const highlightStyle = highlight
     ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'
     : '';
-  const sizeStyle = getContainerSize(size);
+  const { container } = getImageSizeStyle(size);
 
-  return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+  return `${baseStyle} ${highlightStyle} ${container}`;
 }
 
-function getContainerSize(size: AvatarSize): string {
+type ImageSizeStyle = {
+  container: string;
+  image: string;
+};
+function getImageSizeStyle(size: AvatarSize): ImageSizeStyle {
   switch (size) {
     case 'small':
-      return 'w-9 h-9';
+      return {
+        container: 'w-9 h-9',
+        image: 'w-[34px] h-[34px] p-[0.1rem]',
+      };
     case 'medium':
-      return 'w-11 h-11';
+      return {
+        container: 'w-11 h-11',
+        image: 'w-[42px] h-[42px] p-[0.1rem]',
+      };
     case 'large':
-      return 'w-[68px] h-[68px]';
-  }
-}
-
-function getImageSizeStyle(size: AvatarSize): string {
-  switch (size) {
-    case 'small':
-      return 'w-[34px] h-[34px] p-[0.1rem]';
-    case 'medium':
-      return 'w-[42px] h-[42px] p-[0.1rem]';
-    case 'large':
-      return 'w-16 h-16 p-[0.2rem]';
+      return {
+        container: 'w-[68px] h-[68px]',
+        image: 'w-16 h-16 p-[0.2rem]',
+      };
+    case 'xlarge':
+      return {
+        container: 'w-[142px] h-[142px]',
+        image: 'w-[138px] h-[138px] p-[0.3rem]',
+      };
+    default:
+      throw new Error(`Unsupported type size: ${size}`);
   }
 }
--- Frontend/React/DreamCoding/instagram/src/components/UserProfile.tsx
+++ Frontend/React/DreamCoding/instagram/src/components/UserProfile.tsx
@@ -15,20 +15,22 @@ export default function UserProfile({ user }: Props) {
   ];
 
   return (
-    <section>
-      <Avatar image={image} highlignt />
-      <div>
-        <h1>{username}</h1>
-        <FollowButton user={user} />
-        <ul>
+    <section className="w-full flex flex-col md:flex-row items-center justify-center py-12 border-b border-neutral-300">
+      <Avatar image={image} highlignt size="xlarge" />
+      <div className="md:ml-10 basis-1/3">
+        <div className="flex flex-col items-center md:flex-row">
+          <h1 className="text-2xl md:mr-8 my-2 md:mb-0">{username}</h1>
+          <FollowButton user={user} />
+        </div>
+        <ul className="my-4 flex gap-4">
           {info.map(({ title, data }, index) => (
             <li key={index}>
-              <span>{data}</span>
+              <span className="font-bold mr-1">{data}</span>
               {title}
             </li>
           ))}
         </ul>
-        <p>{name}</p>
+        <p className="text-xl font-bold text-center md:text-start">{name}</p>
       </div>
     </section>
   );
