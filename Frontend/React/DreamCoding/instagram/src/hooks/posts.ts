56a856c46a630fade271db82bf68815f520a12eb
Ubar <bellin83@gmail.com>
2023-11-26 06:36:04 +0900

    :necktie: Feat: Revalidate 로직 수정

--- Frontend/React/DreamCoding/instagram/src/hooks/posts.ts
+++ Frontend/React/DreamCoding/instagram/src/hooks/posts.ts
@@ -1,14 +1,36 @@
 import useSWR, { useSWRConfig } from 'swr';
 import { SimplePost } from '@/model/post';
 
+async function updateLike(id: string, like: boolean) {
+  return fetch('api/likes', {
+    method: 'PUT',
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
+
 export default function usePosts() {
-  const { data: posts, isLoading, error } = useSWR<SimplePost[]>('/api/posts');
-  const { mutate } = useSWRConfig();
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>('/api/posts');
   const setLike = (post: SimplePost, username: string, like: boolean) => {
-    fetch('api/likes', {
-      method: 'PUT',
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate('/api/posts'));
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter((item) => item !== username),
+    };
+
+    const newPosts = posts?.map((p) => (p.id === post.id ? newPost : p));
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
   };
   return { posts, isLoading, error, setLike };
 }
