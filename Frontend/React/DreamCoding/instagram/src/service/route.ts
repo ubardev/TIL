bad021216dbe679315b503e5289b1bd4000c72d9
Ubar <bellin83@gmail.com>
2023-11-26 07:04:57 +0900

    :necktie: Feat: bookmark API추가

---
+++ Frontend/React/DreamCoding/instagram/src/service/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { addBookmark, removeBookmark } from '@/service/user';
+import { withSessionUser } from '@/util/session';
+
+export async function PUT(req: NextRequest) {
+  return withSessionUser(async (user) => {
+    const { id, bookmark } = await req.json();
+
+    if (!id || bookmark == null) {
+      return new Response('Bad Request', { status: 400 });
+    }
+
+    const request = bookmark ? addBookmark : removeBookmark;
+
+    return request(user.id, id) //
+      .then((res) => NextResponse.json(res))
+      .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+  });
+}
--- Frontend/React/DreamCoding/instagram/src/service/user.ts
+++ Frontend/React/DreamCoding/instagram/src/service/user.ts
@@ -9,9 +9,7 @@ type OAuthUser = {
   image?: string | null;
 };
 
-export async function addUser(user: OAuthUser) {
-  const { id, username, email, name, image } = user;
-
+export async function addUser({ id, username, email, name, image }: OAuthUser) {
   return client.createIfNotExists({
     _id: id,
     _type: 'user',
@@ -77,4 +75,48 @@ export async function getUserForProfile(username: string) {
       followers: user.followers ?? 0,
       posts: user.posts ?? 0,
     }));
+}
+
+export async function addBookmark(userId: string, postId: string) {
+  return client
+    .patch(userId) //
+    .setIfMissing({ bookmarks: [] })
+    .append('bookmarks', [
+      {
+        _ref: postId,
+        _type: 'reference',
+      },
+    ])
+    .commit({ autoGenerateArrayKeys: true });
+}
+
+export async function removeBookmark(userId: string, postId: string) {
+  return client
+    .patch(userId)
+    .unset([`bookmarks[_ref=="${postId}"]`])
+    .commit();
+}
+
+export async function follow(myId: string, targetId: string) {
+  return client
+    .transaction() //
+    .patch(myId, (user) =>
+      user
+        .setIfMissing({ following: [] })
+        .append('following', [{ _ref: targetId, _type: 'reference' }]),
+    )
+    .patch(targetId, (user) =>
+      user
+        .setIfMissing({ followers: [] })
+        .append('followers', [{ _ref: myId, _type: 'reference' }]),
+    )
+    .commit({ autoGenerateArrayKeys: true });
+}
+
+export async function unfollow(myId: string, targetId: string) {
+  return client
+    .transaction() //
+    .patch(myId, (user) => user.unset([`following[_ref=="${targetId}"]`]))
+    .patch(targetId, (user) => user.unset([`followers[_ref=="${myId}"]`]))
+    .commit({ autoGenerateArrayKeys: true });
 }
