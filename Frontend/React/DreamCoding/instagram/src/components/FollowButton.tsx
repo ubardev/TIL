f0b387930a9c128f7b92ef88b10b9dc55b3fddd5
Ubar <bellin83@gmail.com>
2023-12-05 01:37:35 +0900

    :necktie: Feat: router.refresh 로직 추가

--- Frontend/React/DreamCoding/instagram/src/components/FollowButton.tsx
+++ Frontend/React/DreamCoding/instagram/src/components/FollowButton.tsx
@@ -2,6 +2,9 @@
 
 import useMe from '@/hooks/me';
 import { ProfileUser } from '@/model/user';
+import { useRouter } from 'next/navigation';
+import { useState, useTransition } from 'react';
+import { PulseLoader } from 'react-spinners';
 import Button from './ui/Button';
 
 type Props = {
@@ -11,6 +14,10 @@ type Props = {
 export default function FollowButton({ user }: Props) {
   const { username } = user;
   const { user: loggedInUser, toggleFollow } = useMe();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
 
   const showButton = loggedInUser && loggedInUser.username !== username;
   const following =
@@ -18,14 +25,31 @@ export default function FollowButton({ user }: Props) {
     loggedInUser.following.find((item) => item.username === username);
   const text = following ? 'Unfollow' : 'Follow';
 
-  const handleFollow = () => {
-    toggleFollow(user.id, !following);
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !following);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
   };
 
   return (
     <>
       {showButton && (
-        <Button text={text} onClick={handleFollow} red={text === 'Unfollow'} />
+        <div className="relative">
+          {isUpdating && (
+            <div className="absolute z-20 inset-0 flex justify-center items-center">
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button
+            disabled={isUpdating}
+            text={text}
+            onClick={handleFollow}
+            red={text === 'Unfollow'}
+          />
+        </div>
       )}
     </>
   );
