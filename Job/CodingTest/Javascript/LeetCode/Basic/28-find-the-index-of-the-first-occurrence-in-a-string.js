c6da532fbc8d0f42b79d3f24cee5b725fcf2722d
Ubar <bellin83@gmail.com>
2023-12-08 01:20:56 +0900

    :necktie: Add: 28-find-the-index-of-the-first-occurrence-in-a-string 풀이 추가

---
+++ Job/CodingTest/Javascript/LeetCode/Basic/28-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,8 @@
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+const haystack = "sadbutsad";
+const needle = "sad";
+const result = strStr(haystack, needle);
+console.log(result);
