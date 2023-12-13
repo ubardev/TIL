376b4b2a153681891be7d2b7415121d04f5f8286
Ubar <bellin83@gmail.com>
2023-12-13 23:42:07 +0900

    :necktie: Feat: 66-plus-one 다른 풀이 추가

--- Job/CodingTest/Javascript/LeetCode/Basic/66-plus-one.js
+++ Job/CodingTest/Javascript/LeetCode/Basic/66-plus-one.js
@@ -1,11 +1,25 @@
-var plusOne = function (digits) {
-  const digitsNumber = BigInt(digits.join(""));
+// var plusOne = function (digits) {
+//   const digitsNumber = BigInt(digits.join(""));
 
-  return String(digitsNumber + 1n)
-    .split("")
-    .map(Number);
+//   return String(digitsNumber + 1n)
+//     .split("")
+//     .map(Number);
+// };
+
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i]++;
+      return digits;
+    }
+  }
+
+  digits.unshift(1);
+  return digits;
 };
 
-const params = [1, 2, 3];
+const params = [9, 9];
 const result = plusOne(params);
 console.log(result);
