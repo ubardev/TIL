e31880cb4c811bc074b94df8fb89067b14c27871
Ubar <bellin83@gmail.com>
2023-12-19 16:26:40 +0900

    :necktie: Add: 05-이진트리 너비우선탐색(BFS) 풀이 추가

---
+++ Job/CodingTest/Javascript/Inflearn/09-그래프와탐색/05-이진트리 너비우선탐색(BFS).js
@@ -0,0 +1,20 @@
+function solution() {
+  let answer = "";
+  let queue = [];
+  queue.push(1);
+
+  while (queue.length) {
+    let v = queue.shift();
+    answer += v + " ";
+
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
+    }
+  }
+
+  return answer;
+}
+
+let result = solution();
+console.log(result);
