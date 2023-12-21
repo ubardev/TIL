85d964a63d92bb7adb7aeea58b50d1b868203b33
Ubar <bellin83@gmail.com>
2023-12-21 23:59:13 +0900

    :necktie: Feat: 다시 풀이

--- Job/CodingTest/Javascript/Inflearn/09-그래프와탐색/05-이진트리 너비우선탐색(BFS).js
+++ Job/CodingTest/Javascript/Inflearn/09-그래프와탐색/05-이진트리 너비우선탐색(BFS).js
@@ -5,7 +5,7 @@ function solution() {
 
   while (queue.length) {
     let v = queue.shift();
-    answer += v + " ";
+    answer += v + "  ";
 
     for (let nv of [v * 2, v * 2 + 1]) {
       if (nv > 7) continue;
