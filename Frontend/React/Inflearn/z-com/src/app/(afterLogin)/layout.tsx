0f819d4b9d6e0cdb2dbc8f676df0cd815a4d88f9
Ubar <bellin83@gmail.com>
2024-01-06 22:29:03 +0900

    :necktie: Add: useSelectedLayoutSegment로 ActiveLink 만들기

--- Frontend/React/Inflearn/z-com/src/app/(afterLogin)/layout.tsx
+++ Frontend/React/Inflearn/z-com/src/app/(afterLogin)/layout.tsx
@@ -3,6 +3,8 @@ import style from "@/app/(afterLogin)/layout.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import ZLogo from "/public/zlogo.png";
+import NavMenu from "./_component/NavMenu";
+import LogoutButton from "./_component/LogoutButton";
 
 export default function AfterLoginLayout({
   children,
@@ -19,6 +21,15 @@ export default function AfterLoginLayout({
                 <Image src={ZLogo} alt="z.com로고" width={40} height={40} />
               </div>
             </Link>
+            <nav>
+              <ul>
+                <NavMenu />
+              </ul>
+              <Link href="/compose/tweet" className={style.postButton}>
+                게시하기
+              </Link>
+            </nav>
+            <LogoutButton />
           </div>
         </section>
       </header>
