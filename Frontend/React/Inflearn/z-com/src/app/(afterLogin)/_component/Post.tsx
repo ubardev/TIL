8ae7c45a89fa46bbdcada9a057fc095bd4ba5eb9
Ubar <bellin83@gmail.com>
2024-01-11 06:29:51 +0900

    :necktie: Add: classnames로 클래스 합성하기(feat. npmtrends로 라이브러리 고르기)

--- Frontend/React/Inflearn/z-com/src/app/(afterLogin)/_component/Post.tsx
+++ Frontend/React/Inflearn/z-com/src/app/(afterLogin)/_component/Post.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
-// import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
@@ -42,7 +42,7 @@ export default function Post() {
           </div>
           <div>{target.content}</div>
           <div className={style.postImageSection}></div>
-          {/* <ActionButtons /> */}
+          <ActionButtons />
         </div>
       </div>
     </article>
