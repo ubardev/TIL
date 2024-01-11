26bbb90d1ca60e564c5ccdce9257cd237979490f
Ubar <bellin83@gmail.com>
2024-01-12 07:53:15 +0900

    :necktie: Add: compose tweet modal 파일 추가

--- Frontend/React/Inflearn/z-com/src/app/(afterLogin)/layout.tsx
+++ Frontend/React/Inflearn/z-com/src/app/(afterLogin)/layout.tsx
@@ -8,11 +8,12 @@ import LogoutButton from "./_component/LogoutButton";
 import TrendSection from "./_component/TrendSection";
 import FollowRecommend from "./_component/FollowRecommend";
 
-export default function AfterLoginLayout({
-  children,
-}: {
+type Props = {
   children: ReactNode;
-}) {
+  modal: ReactNode;
+};
+
+export default function AfterLoginLayout({ children, modal }: Props) {
   return (
     <div className={style.container}>
       <header className={style.leftSectionWrapper}>
@@ -59,6 +60,7 @@ export default function AfterLoginLayout({
           </section>
         </div>
       </div>
+      {modal}
     </div>
   );
 }
