5a71c4dcc779e71473a70af7acc809ee71e9a6dd
ubardev <bellin83@gmail.com>
2024-02-04 04:29:42 +0900

    :necktie: Feat: 스크롤 효과 적용

--- Frontend/Javascript/Inflearn/Interactive/section-11/js/wall3d.js
+++ Frontend/Javascript/Inflearn/Interactive/section-11/js/wall3d.js
@@ -1,7 +1,16 @@
 (function () {
   const houseElem = document.querySelector(".house");
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - this.window.innerHeight;
+  }
 
   window.addEventListener("scroll", function () {
-    console.log(window.pageYOffset);
+    const zMove = (this.pageYOffset / maxScrollValue) * 980 - 490;
+    houseElem.style.transform = `translateZ(${zMove}vw)`;
   });
+
+  window.addEventListener("resize", resizeHandler);
+  resizeHandler();
 })();
