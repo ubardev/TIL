0a1fed53586e6071220b491ce61305f32bdd92d4
ubardev <bellin83@gmail.com>
2024-02-05 05:32:43 +0900

    :necktie: Feat: 스크롤바 추가

--- Frontend/Javascript/Inflearn/Interactive/section-11/js/wall3d.js
+++ Frontend/Javascript/Inflearn/Interactive/section-11/js/wall3d.js
@@ -1,5 +1,6 @@
 (function () {
   const houseElem = document.querySelector(".house");
+  const barElem = document.querySelector(".progress-bar");
   let maxScrollValue;
 
   function resizeHandler() {
@@ -7,8 +8,12 @@
   }
 
   window.addEventListener("scroll", function () {
-    const zMove = (this.pageYOffset / maxScrollValue) * 980 - 490;
+    const scrollPer = this.pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`;
+
+    // progress bar
+    barElem.style.width = `${scrollPer * 100}%`;
   });
 
   window.addEventListener("resize", resizeHandler);
