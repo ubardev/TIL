16f82f3d0a05885b6743dd0123b982f3f15a22ac
ubardev <bellin83@gmail.com>
2024-02-06 08:08:36 +0900

    :necktie: Feat: 마우스 커서에 따른 시점 변화 기능 추가

--- Frontend/Javascript/Inflearn/Interactive/section-11/js/wall3d.js
+++ Frontend/Javascript/Inflearn/Interactive/section-11/js/wall3d.js
@@ -1,6 +1,8 @@
 (function () {
+  const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
   const barElem = document.querySelector(".progress-bar");
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue;
 
   function resizeHandler() {
@@ -14,6 +16,14 @@
 
     // progress bar
     barElem.style.width = `${scrollPer * 100}%`;
+  });
+
+  window.addEventListener("mousemove", function (e) {
+    mousePos.x = -1 + (e.clientX / this.window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / this.window.innerHeight) * 2;
+    stageElem.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${
+      mousePos.x * 5
+    }deg)`;
   });
 
   window.addEventListener("resize", resizeHandler);
