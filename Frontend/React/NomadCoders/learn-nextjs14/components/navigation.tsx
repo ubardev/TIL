2c1d42e3023d2601c3546b931af9d924e357622a
ubardev <bellin83@gmail.com>
2024-02-09 05:25:57 +0900

    :necktie: Add: #2.2 Not Found Routes

---
+++ Frontend/React/NomadCoders/learn-nextjs14/components/navigation.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+  console.log(path);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+          {path === "/" ? "🔥" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link>
+          {path === "/about-us" ? "🔥" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
+}
