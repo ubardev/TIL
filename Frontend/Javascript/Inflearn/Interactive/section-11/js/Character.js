dfe5c2ec8fc7f947fab232667dac6317071c2c01
ubardev <bellin83@gmail.com>
2024-02-10 06:24:50 +0900

    :necktie: Feat: 팔다리 움직이는 애니메이션 추가

--- Frontend/Javascript/Inflearn/Interactive/section-11/js/Character.js
+++ Frontend/Javascript/Inflearn/Interactive/section-11/js/Character.js
@@ -1,6 +1,6 @@
 function Character(info) {
   this.mainElem = document.createElement("div");
-  this.mainElem.classList.add("character");
+  this.mainElem.classList.add("character", "running");
   this.mainElem.innerHTML = `
           <div class="character-face-con character-head">
             <div class="character-face character-head-face face-front"></div>
@@ -30,3 +30,7 @@ function Character(info) {
   document.querySelector(".stage").appendChild(this.mainElem);
   this.mainElem.style.left = info.xPos + "%";
 }
+
+Character.prototype = {
+  constructor: Character,
+};
