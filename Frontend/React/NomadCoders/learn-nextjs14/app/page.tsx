de13665a8a83fe20b2097a0ad3b03947a2e37c73
ubardev <bellin83@gmail.com>
2024-02-11 23:26:30 +0900

    :necktie: #2.7 Layouts

--- Frontend/React/NomadCoders/learn-nextjs14/app/page.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "../components/navigation";
-
 export default function Tomato() {
   return (
     <div>
-      <Navigation />
       <h1>Hello!</h1>
     </div>
   );
--- Frontend/React/NomadCoders/learn-nextjs14/components/navigation.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/components/navigation.tsx
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation";
 
 export default function Navigation() {
   const path = usePathname();
-  console.log(path);
 
   return (
     <nav>
