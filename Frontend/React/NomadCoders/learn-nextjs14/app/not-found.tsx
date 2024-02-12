0d3fcd29f519041ec05574e18c9a7bc539a43330
ubardev <bellin83@gmail.com>
2024-02-13 04:36:15 +0900

    :necktie: #2.8 Metadata

--- Frontend/React/NomadCoders/learn-nextjs14/app/not-found.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/app/not-found.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import Navigation from "../components/navigation";
+
+export const metadata: Metadata = {
+  title: "Not found",
+};
 
 export default function NotFound() {
   return (
