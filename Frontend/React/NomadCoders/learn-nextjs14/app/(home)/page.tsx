534d34602babdf5280cc876d8b05ba2bdbe70455
ubardev <bellin83@gmail.com>
2024-02-14 08:52:54 +0900

    :technologist: Add: #3.4 Parallel Requests

--- Frontend/React/NomadCoders/learn-nextjs14/app/(home)/page.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/app/(home)/page.tsx
@@ -1,17 +1,27 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  return fetch(URL).then((response) => response.json());
+  return fetch(API_URL).then((response) => response.json());
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
 
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- Frontend/React/NomadCoders/learn-nextjs14/app/(movies)/movies/[id]/page.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>Movie {id}</div>;
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+
+  return <div>{movie.title}</div>;
 }
