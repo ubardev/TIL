a9f8e480f78666cb8c3e602051d3d7bc6b12e2f4
ubardev <bellin83@gmail.com>
2024-02-14 07:45:08 +0900

    :necktie: Add: #3.1 Client Side

--- Frontend/React/NomadCoders/learn-nextjs14/app/(home)/page.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/app/(home)/page.tsx
@@ -1,11 +1,27 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//   title: "Home",
+// };
 
 export default function Tomato() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
