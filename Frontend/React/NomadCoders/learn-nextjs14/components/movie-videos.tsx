7419acae8158577cc730cab6b81079b2a1896c3a
ubardev <bellin83@gmail.com>
2024-02-14 09:16:44 +0900

    :necktie: Add: #3.7 Error Handling

--- Frontend/React/NomadCoders/learn-nextjs14/components/movie-videos.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/components/movie-videos.tsx
@@ -3,8 +3,10 @@ import { API_URL } from "../app/(home)/page";
 async function getVideos(id: string) {
   await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
+  throw new Error("something broke...");
+
+  //   const response = await fetch(`${API_URL}/${id}/videos`);
+  //   return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
