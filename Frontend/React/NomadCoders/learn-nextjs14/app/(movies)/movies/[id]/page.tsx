b0a6d52051de36361016f6f1f71aebda71d29eec
ubardev <bellin83@gmail.com>
2024-02-14 09:06:51 +0900

    :necktie: Feat: #3.5 Suspense

--- Frontend/React/NomadCoders/learn-nextjs14/app/(movies)/movies/[id]/page.tsx
+++ Frontend/React/NomadCoders/learn-nextjs14/app/(movies)/movies/[id]/page.tsx
@@ -1,25 +1,21 @@
-import { API_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  const response = await fetch(`${API_URL}/${id}`);
-  return response.json();
-}
-
-async function getVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-
-  return <div>{movie.title}</div>;
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
---
+++ Frontend/React/NomadCoders/learn-nextjs14/components/movie-videos.tsx
@@ -0,0 +1,14 @@
+import { API_URL } from "../app/(home)/page";
+
+async function getVideos(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideos(id);
+
+  return <h6>{JSON.stringify(videos)}</h6>;
+}
