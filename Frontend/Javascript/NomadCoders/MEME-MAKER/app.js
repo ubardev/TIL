1788e948e2d76b8a247706e57e71ab39c20cbaa1
ubardev <bellin83@gmail.com>
2024-02-26 07:59:22 +0900

    :necktie: #1.1 Our First Drawing

--- Frontend/Javascript/NomadCoders/MEME-MAKER/app.js
+++ Frontend/Javascript/NomadCoders/MEME-MAKER/app.js
@@ -1,1 +1,7 @@
-alert("hello!");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(50, 50, 100, 200);
