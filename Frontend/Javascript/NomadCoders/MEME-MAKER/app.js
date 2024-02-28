907133fab06c0a90112555cba570212700e87659
ubardev <bellin83@gmail.com>
2024-02-28 09:33:12 +0900

    :necktie: 집 그리기 연습

--- Frontend/Javascript/NomadCoders/MEME-MAKER/app.js
+++ Frontend/Javascript/NomadCoders/MEME-MAKER/app.js
@@ -4,9 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.strokeRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fill();
