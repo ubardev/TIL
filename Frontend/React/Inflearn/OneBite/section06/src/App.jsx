9e0ee101e3d5050300609f9401178513cd8e549d
ubardev <bellin83@gmail.com>
2024-03-12 08:22:35 +0900

    :necktie: 6.3) 기능 구현하기

--- Frontend/React/Inflearn/OneBite/section06/src/App.jsx
+++ Frontend/React/Inflearn/OneBite/section06/src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller onClickButton={onClickButton} />
       </section>
     </div>
   );
