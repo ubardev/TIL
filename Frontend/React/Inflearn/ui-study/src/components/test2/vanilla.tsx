1eb66f037c9dfb00ec067ce5d4b26e0eefc77fe9
ubardev <bellin83@gmail.com>
2024-03-30 05:50:00 +0900

    :necktie: Feat: 바닐라 동작 영억 추가

--- Frontend/React/Inflearn/ui-study/src/components/test2/vanilla.tsx
+++ Frontend/React/Inflearn/ui-study/src/components/test2/vanilla.tsx
@@ -1,3 +1,24 @@
-const Test2_Vanilla = () => <div>테스트2 - 바닐라</div>;
+import VanillaWrapper from "@/components/vanillaWrapper";
+import { createElement } from "react";
+
+const initiator = (wrapper: HTMLDivElement) => {
+  let state = false;
+  const pElem = document.createElement("p");
+  pElem.textContent = "꺼짐";
+  const btnElem = document.createElement("button");
+  btnElem.textContent = "토글";
+  btnElem.addEventListener("click", () => {
+    state = !state;
+    pElem.textContent = state ? "켜짐" : "꺼짐";
+  });
+
+  const divElem = document.createElement("div");
+  divElem.textContent = "테스트2 - 바닐라";
+  divElem.append(btnElem, pElem);
+
+  wrapper.insertAdjacentElement("beforeend", divElem);
+};
+
+const Test2_Vanilla = () => <VanillaWrapper initiator={initiator} />;
 
 export default Test2_Vanilla;
