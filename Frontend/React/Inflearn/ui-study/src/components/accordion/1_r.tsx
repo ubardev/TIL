a5f0e01b91b453f3a32de5ed310b4b7c3ef9d68e
ubardev <bellin83@gmail.com>
2024-05-27 23:02:53 +0900

    :necktie: 아코디언 (1/6) 보이는 요소만 렌더링

--- Frontend/React/Inflearn/ui-study/src/components/accordion/1_r.tsx
+++ Frontend/React/Inflearn/ui-study/src/components/accordion/1_r.tsx
@@ -1,15 +1,50 @@
 import { data } from "@/components/accordion/data";
+import cx from "./cx";
+import { useState } from "react";
+
+const AccordionItem = ({
+  id,
+  title,
+  description,
+  current,
+  toggle,
+}: {
+  id: string;
+  title: string;
+  description: string;
+  current: boolean;
+  toggle: () => void;
+}) => {
+  return (
+    <li className={cx("item", { current })} key={id}>
+      <div className={cx("tab")} onClick={toggle}>
+        {title}
+      </div>
+      {current ? <div className={cx("description")}>{description}</div> : null}
+    </li>
+  );
+};
 
 const Accordion1 = () => {
+  const [currentId, setCurrentId] = useState<string | null>(data[0].id);
+
+  const toggleItem = (id: string) => () => {
+    setCurrentId((prev) => (prev === id ? null : id));
+  };
+
   return (
     <>
       <h3>#1. React</h3>
-      {data.map((d: any) => (
-        <div>
-          {d.title}
-          {d.description}
-        </div>
-      ))}
+      <ul className={cx("container")}>
+        {data.map((d: any) => (
+          <AccordionItem
+            key={d.id}
+            {...d}
+            current={currentId === d.id}
+            toggle={toggleItem(d.id)}
+          />
+        ))}
+      </ul>
     </>
   );
 };
