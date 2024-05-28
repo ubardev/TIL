4c3108986189e11c49a4ccc57bcb3e61c161824f
ubardev <bellin83@gmail.com>
2024-05-28 22:45:18 +0900

    :necktie: 아코디언 (2/6) css로 숨김/보임 처리

--- Frontend/React/Inflearn/ui-study/src/components/accordion/index.tsx
+++ Frontend/React/Inflearn/ui-study/src/components/accordion/index.tsx
@@ -1,11 +1,13 @@
 import cx from "@/components/accordion/cx";
 import Accordion1 from "@/components/accordion/1_r";
+import Accordion2 from "@/components/accordion/2_r";
 
 const Accordions = () => {
   return (
     <div className={cx("Accordions")}>
       <h2>아코디언</h2>
       <Accordion1 />
+      <Accordion2 />
     </div>
   );
 };
