429e8d1495f85f46a13d9621a5095828d6423587
ubardev <bellin83@gmail.com>
2024-06-12 08:08:09 +0900

    :necktie: Feat: 탭메뉴 개발 기본 파일 셋팅

---
+++ Frontend/React/Inflearn/ui-study/src/components/tabMenu/index.tsx
@@ -0,0 +1,11 @@
+import cx from "@/components/accordion/cx";
+
+const TabMenus = () => {
+  return (
+    <div className={cx("TabMenus")}>
+      <h2>탭메뉴</h2>
+    </div>
+  );
+};
+
+export default TabMenus;
