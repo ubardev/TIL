1d697fe96d68be6c3cea953afcc19ac7d24e3f8e
ubardev <bellin83@gmail.com>
2024-06-14 07:41:41 +0900

    :necktie: 탭메뉴 (2/4) css로 숨김/보임 처리

--- Frontend/React/Inflearn/ui-study/src/components/tabMenu/index.tsx
+++ Frontend/React/Inflearn/ui-study/src/components/tabMenu/index.tsx
@@ -1,11 +1,13 @@
 import cx from "@/components/tabMenu/cx";
 import TabMenu1 from "@/components/tabMenu/1_r";
+import TabMenu2 from "@/components/tabMenu/2_r";
 
 const TabMenus = () => {
   return (
     <div className={cx("TabMenus")}>
       <h2>탭메뉴</h2>
       <TabMenu1 />
+      <TabMenu2 />
     </div>
   );
 };
