ad72e38275e32ba3b30c2bfc61c536a2c8c354ff
ubardev <bellin83@gmail.com>
2024-06-17 07:19:32 +0900

    :necktie: 탭메뉴 (3/4) 단일 트리 형태의 html로 작성

---
+++ Frontend/React/Inflearn/ui-study/src/components/tabMenu/3_r.tsx
@@ -0,0 +1,56 @@
+import data from "./data";
+import cx from "./cx";
+import { useState } from "react";
+
+const TabItem = ({
+  id,
+  title,
+  current,
+  toggle,
+}: {
+  id: string;
+  title: string;
+  current: boolean;
+  toggle: () => void;
+}) => {
+  return (
+    <li className={cx("tab", { current })} key={id} onClick={toggle}>
+      {title}
+    </li>
+  );
+};
+
+const TabMenu2 = () => {
+  const [currentId, setCurrentId] = useState<string | null>(data[0].id);
+
+  const toggleItem = (id: string) => () => {
+    setCurrentId((prev) => (prev === id ? null : id));
+  };
+
+  return (
+    <>
+      <h3>
+        #1. React<sub>다 그려놓고 hidden / show css로 처리</sub>
+      </h3>
+      <div className={cx("container", "tabMenu2")}>
+        <div className={cx("tabList")}>
+          {data.map((d: any) => (
+            <TabItem
+              key={d.id}
+              {...d}
+              current={currentId === d.id}
+              toggle={toggleItem(d.id)}
+            />
+          ))}
+        </div>
+        {data.map((d) => (
+          <div className={cx("description", { current: currentId === d.id })}>
+            {d.description || ""}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TabMenu2;
