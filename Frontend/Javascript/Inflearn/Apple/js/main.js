e1ca88b04c93c704ab77adb5f733ee7114e21799
ubardev <bellin83@gmail.com>
2024-06-30 06:28:34 +0900

    :necktie: 현재 활성시킬 씬 결정하기

--- Frontend/Javascript/Inflearn/Apple/js/main.js
+++ Frontend/Javascript/Inflearn/Apple/js/main.js
@@ -1,5 +1,7 @@
 (() => {
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이 값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
 
   const sceneInfo = [
     {
@@ -49,7 +51,23 @@
     }
   }
 
-  function scrollLoop() {}
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+    }
+
+    console.log("currentScene >>>>>>>>>> ", currentScene);
+  }
 
   window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
