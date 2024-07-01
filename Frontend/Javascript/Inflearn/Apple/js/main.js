168914eeb7bb37868ecc26a7e7bd9687ef438f25
ubardev <bellin83@gmail.com>
2024-07-02 07:38:31 +0900

    :necktie: Feat: 특정 타이밍 스크롤 애니메이션 기능 추가

--- Frontend/Javascript/Inflearn/Apple/js/main.js
+++ Frontend/Javascript/Inflearn/Apple/js/main.js
@@ -80,9 +80,33 @@
   function calcValues(values, currentYOffset) {
     let rv;
     // 현재 씬에서 스크롤된 범위를 비율로 구하기
-    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    let scrollRatio = currentYOffset / scrollHeight;
 
-    rv = scrollRatio * (values[1] - values[0]) + values[0];
+    if (values.length === 3) {
+      // start ~ end 사이에 애니메이션 실행
+      const partScrollStart =
+        values[2].start * sceneInfo[currentScene].scrollHeight;
+      const partScrollEnd =
+        values[2].end * sceneInfo[currentScene].scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
 
     return rv;
   }
