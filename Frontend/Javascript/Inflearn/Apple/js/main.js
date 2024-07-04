ef3d5378fdd59ec236e615d3e950ac16b08fcd0e
ubardev <bellin83@gmail.com>
2024-07-05 07:42:32 +0900

    :necktie: Feat: 비디오 인터렉션 적용 1

--- Frontend/Javascript/Inflearn/Apple/js/main.js
+++ Frontend/Javascript/Inflearn/Apple/js/main.js
@@ -16,8 +16,13 @@
         messageB: document.querySelector("#scroll-section-0 .main-message.b"),
         messageC: document.querySelector("#scroll-section-0 .main-message.c"),
         messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+        canvas: document.querySelector("#video-canvas-0"),
+        context: document.querySelector("#video-canvas-0").getContext("2d"),
+        videoImages: [],
       },
       values: {
+        videoImageCount: 300,
+        imageSequence: [0, 299],
         messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
         messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
         messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -92,6 +97,16 @@
       values: {},
     },
   ];
+
+  function setCanvasImages() {
+    let imgElem;
+    for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+      imgElem = new Image();
+      imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+      sceneInfo[0].objs.videoImages.push(imgElem);
+    }
+  }
+  setCanvasImages();
 
   function setLayout() {
     // 각 스크롤 섹션의 높이 셋팅
