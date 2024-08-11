09cb9f623c84befa74e2586e08b49087a129fc6e
ubardev <bellin83@gmail.com>
2024-08-12 08:19:24 +0900

    :necktie: Feat: 아코디언 (1/6) 보이는 요소만 렌더링

---
+++ Frontend/React/Inflearn/ui-study-start/src/components/accordion/data.ts
@@ -0,0 +1,43 @@
+const data = [
+  {
+    id: '6573c0e5001221fbdec2f76c',
+    title:
+      'Adipisicing aliqua ex magna cupidatat sit ea officia eiusmod aliqua tempor amet non. Quis reprehenderit non cupidatat ipsum irure.',
+    description:
+      'Nisi et sit in cillum anim do. Anim ullamco eu elit aute pariatur dolore cupidatat nostrud. Eiusmod eiusmod sunt deserunt velit laboris consequat et aute velit exercitation velit. Incididunt deserunt mollit aute aliqua et dolor. Eiusmod officia exercitation tempor cillum occaecat sunt qui proident est. Enim fugiat labore sint irure nisi. Eu mollit id consectetur aute dolor.\r\nAute cupidatat aliquip veniam mollit aute velit ex fugiat aute elit. Officia tempor cillum nisi eu. Qui aliquip labore laboris incididunt anim laboris anim excepteur exercitation. Quis minim labore elit reprehenderit id.\r\n',
+  },
+  {
+    id: '6573c0e51c767e04b752db44',
+    title: 'Fugiat mollit deserunt pariatur labore laboris adipisicing.',
+    description:
+      'Exercitation voluptate consequat officia cupidatat mollit sunt. Nisi exercitation dolore duis est non ex. Sit anim ad ipsum mollit do minim.\r\nQui do sunt eu minim tempor esse enim esse sit id pariatur labore deserunt. Ex in ad et aute veniam aliquip est deserunt reprehenderit dolor do officia proident ad. Aliqua ad proident est magna deserunt. Et culpa sint excepteur ex esse commodo irure aliquip elit.\r\n',
+  },
+  {
+    id: '6573c0e5d717806a6ad8c25c',
+    title:
+      'Sit et nulla ut veniam officia excepteur amet fugiat ea in dolor consectetur laborum aute. Aute esse irure non in.',
+    description:
+      'Fugiat aute officia magna ea ad et sint laborum consequat nisi reprehenderit qui laborum consequat. Minim excepteur enim fugiat qui Lorem ullamco aliquip do. Est ut in duis consectetur voluptate eiusmod in. Dolore ut eiusmod ipsum excepteur excepteur excepteur amet sunt dolore nulla exercitation duis ex labore.\r\nUllamco quis excepteur excepteur officia ad voluptate mollit ut mollit ex commodo officia quis mollit. Nulla exercitation tempor amet tempor officia ex minim laboris. Id tempor ipsum amet commodo veniam fugiat aute consectetur pariatur do laboris mollit.\r\nExcepteur occaecat dolore consectetur non fugiat aliqua aliqua cupidatat anim veniam pariatur commodo. Fugiat ullamco veniam irure exercitation exercitation. Reprehenderit et reprehenderit aliquip pariatur commodo nostrud veniam commodo. Anim non irure occaecat culpa qui ullamco qui est amet cupidatat sunt consequat voluptate. Ad officia veniam ad sunt Lorem. Sint velit ipsum velit voluptate nostrud in duis enim laborum.\r\n',
+  },
+  {
+    id: '6573c0e54bdb722ed86ee42d',
+    title: 'Cillum nisi ipsum laborum dolor eiusmod deserunt amet duis ea Lorem sit id dolor.',
+    description:
+      'Sunt dolore nostrud laborum eiusmod est amet non nostrud consectetur. Qui quis reprehenderit ad ipsum ea et esse reprehenderit cupidatat minim qui proident officia. Ea mollit aliqua nisi consequat adipisicing. Culpa laboris nulla occaecat sunt nulla anim nulla do cillum cupidatat veniam. Ut cillum ipsum officia duis ea est elit occaecat elit duis proident.\r\nUllamco qui adipisicing sint eu id aliqua qui sit duis anim irure quis. Reprehenderit in minim eu nostrud incididunt duis minim id sit. Esse reprehenderit aute velit cillum eiusmod aliquip. Commodo ut Lorem minim sint do ipsum adipisicing adipisicing aliquip nostrud. Culpa veniam elit nostrud ullamco.\r\n',
+  },
+  {
+    id: '6573c0e5bac5e8cb6c91fbc1',
+    title: 'Reprehenderit tempor irure et est.',
+    description:
+      'Ex veniam cillum sit cupidatat cillum. Laborum est sunt enim incididunt laboris culpa. Sint anim ullamco anim ea cillum. Fugiat quis minim anim occaecat voluptate do duis do consectetur nisi labore esse nulla commodo.\r\nMollit sit laboris ullamco ullamco occaecat fugiat proident. Dolore eiusmod mollit ea elit irure aliqua veniam. In irure enim fugiat ut magna veniam ipsum cupidatat ea id sit.\r\nEa cupidatat exercitation in incididunt magna. Adipisicing aliquip do et aliquip fugiat eiusmod quis velit non quis. Est consequat ullamco officia fugiat laborum consequat ad labore veniam sint occaecat qui. Mollit consequat veniam consectetur nostrud commodo mollit cupidatat culpa do veniam ipsum dolor ullamco tempor. Sunt sunt ad nulla irure adipisicing non labore labore ipsum labore mollit laboris exercitation non. Ipsum reprehenderit ad ut laboris sunt occaecat laboris velit reprehenderit. Sint magna adipisicing laboris qui aute dolor do.\r\n',
+  },
+  {
+    id: '6573c0e52d221dc14a49a9a7',
+    title:
+      'Aliqua magna proident sint ipsum incididunt eiusmod sunt amet qui cupidatat nulla. Dolor ullamco est aliquip aliquip irure exercitation ad cupidatat ipsum pariatur irure adipisicing in.',
+    description:
+      'Cupidatat ullamco enim officia ea esse ut exercitation commodo nisi aute dolor enim reprehenderit. Adipisicing cupidatat dolore fugiat mollit ipsum ad labore. Non officia anim tempor occaecat eiusmod commodo consequat ipsum. Labore laborum elit ex velit reprehenderit enim. Mollit pariatur ullamco in deserunt fugiat sunt sint duis cupidatat labore. Enim irure sunt eiusmod minim ad est ex proident cillum sit occaecat non. Excepteur reprehenderit nostrud incididunt ut deserunt consectetur laborum culpa velit labore nisi laborum.\r\nCillum sit pariatur duis sunt deserunt magna magna aute incididunt elit ut aliquip deserunt consectetur. Ullamco cupidatat consequat aute ullamco dolor eu consectetur occaecat occaecat laborum dolor amet deserunt. Ipsum dolor occaecat do aliquip ipsum. Aliquip ex ut voluptate sint commodo ad nostrud do Lorem anim minim occaecat amet excepteur. Quis magna laboris minim pariatur. Laborum aliquip adipisicing nostrud ut nostrud.\r\n',
+  },
+]
+
+export default data
--- Frontend/React/Inflearn/ui-study-start/src/components/accordion/index.tsx
+++ Frontend/React/Inflearn/ui-study-start/src/components/accordion/index.tsx
@@ -1,5 +1,12 @@
+import cx from './cx'
+import Accordion1 from "@/components/accordion/1_r";
+
 const Accordions = () => {
-    return <></>
+    return (
+        <div className={cx('Accordions')}>
+            <h2>아코디언</h2>
+            <Accordion1/>
+        </div>)
 }
 
 export default Accordions;
