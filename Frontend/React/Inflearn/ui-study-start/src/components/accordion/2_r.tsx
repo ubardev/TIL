1c51674e5b4d26008cb96cc6caba3e579bd3c420
ubardev <bellin83@gmail.com>
2024-08-13 07:49:02 +0900

    :necktie: Feat: 아코디언(2/6) css로 숨김 / 보임 처리

---
+++ Frontend/React/Inflearn/ui-study-start/src/components/accordion/2_r.tsx
@@ -0,0 +1,51 @@
+import data from '@/components/accordion/data';
+import cx from '@/components/accordion/cx';
+import { useState } from 'react';
+
+const AccordionItem = ({
+  id,
+  title,
+  description,
+  current,
+  toggle,
+}: {
+  id: string;
+  title: string;
+  description: string;
+  current: boolean;
+  toggle: () => void;
+}) => {
+  return (
+    <li className={cx('item', 'item2', { current })} key={id}>
+      <div className={cx('tab')} onClick={toggle}>
+        {title}
+      </div>
+      <div className={cx('description')}>{description}</div>
+    </li>
+  );
+};
+
+const Accordion2 = () => {
+  const [currentId, setCurrentId] = useState<string | null>(data[0].id);
+  const toggleItem = (id: string) => () => {
+    setCurrentId((prev) => (prev === id ? null : id));
+  };
+
+  return (
+    <>
+      <h3>#2. React</h3>
+      <ul className={cx('container')}>
+        {data.map((d) => (
+          <AccordionItem
+            {...d}
+            key={d.id}
+            current={currentId === d.id}
+            toggle={toggleItem(d.id)}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Accordion2;
--- Frontend/React/Inflearn/ui-study-start/src/components/accordion/index.tsx
+++ Frontend/React/Inflearn/ui-study-start/src/components/accordion/index.tsx
@@ -1,12 +1,15 @@
-import cx from './cx'
-import Accordion1 from "@/components/accordion/1_r";
+import cx from './cx';
+import Accordion1 from '@/components/accordion/1_r';
+import Accordion2 from '@/components/accordion/2_r';
 
 const Accordions = () => {
-    return (
-        <div className={cx('Accordions')}>
-            <h2>아코디언</h2>
-            <Accordion1/>
-        </div>)
-}
+  return (
+    <div className={cx('Accordions')}>
+      <h2>아코디언</h2>
+      <Accordion1 />
+      <Accordion2 />
+    </div>
+  );
+};
 
 export default Accordions;
