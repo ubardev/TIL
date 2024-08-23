16ec2efa9fec5d1ca4cf141005afec7fcb724f90
ubardev <bellin83@gmail.com>
2024-08-23 15:51:33 +0900

    :necktie: Feat: 툴팁 외부 클릭시 닫히도록 처리

--- Frontend/React/Inflearn/ui-study-start/src/components/tooltip/1_r.tsx
+++ Frontend/React/Inflearn/ui-study-start/src/components/tooltip/1_r.tsx
@@ -1,6 +1,6 @@
 import data from './data';
 import cx from '@/components/tooltip/cx';
-import { SyntheticEvent, useState } from 'react';
+import { SyntheticEvent, useEffect, useState } from 'react';
 
 const Tooltip = ({
   id,
@@ -15,14 +15,31 @@ const Tooltip = ({
 
   const handleClick = (e: SyntheticEvent) => {
     e.stopPropagation();
+    toggle((p) => !p);
   };
+
+  useEffect(() => {
+    const close = () => toggle(false);
+
+    if (isOpen) {
+      window.addEventListener('click', close);
+    }
+
+    return () => {
+      window.removeEventListener('click', close);
+    };
+  }, [isOpen]);
 
   return (
     <div className={cx('container')}>
-      <button className={cx('trigger')} onClick={() => toggle((p) => !p)}>
+      <button className={cx('trigger')} onClick={handleClick}>
         {title}
       </button>
-      {isOpen && <div className={cx('tooltip')}>{description}</div>}
+      {isOpen && (
+        <div className={cx('tooltip')} onClick={(e) => e.stopPropagation()}>
+          {description}
+        </div>
+      )}
     </div>
   );
 };
@@ -30,7 +47,9 @@ const Tooltip = ({
 const Tooltip1 = () => {
   return (
     <>
-      <h3>#1. React</h3>
+      <h3>
+        #1. React<sub>외부 클릭시 닫히도록 처리</sub>
+      </h3>
       {data.map((d) => (
         <Tooltip {...d} key={d.id} />
       ))}
