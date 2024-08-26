627d4a768b542e331cbf26a6bc23754edcb4c314
ubardev <bellin83@gmail.com>
2024-08-27 07:59:17 +0900

    :necktie: Feat: 툴팁 (3/6) HTML만으로 작성

---
+++ Frontend/React/Inflearn/ui-study-start/src/components/tooltip/3_r.tsx
@@ -0,0 +1,54 @@
+import data from '@/components/tooltip/data';
+import { SyntheticEvent, useEffect, useState } from 'react';
+import cx from '@/components/tooltip/cx';
+
+const Tooltip = ({
+  id,
+  title,
+  description,
+}: {
+  id: string;
+  title: string;
+  description: string;
+}) => {
+  return (
+    <details className={cx('details')} data-tooltip={id}>
+      <summary className={cx('summary')} data-tooltip-summary>
+        {title}
+      </summary>
+      <div className={cx('tooltip')} onClick={(e) => e.stopPropagation()}>
+        {description}
+      </div>
+    </details>
+  );
+};
+
+const Tooltip3 = () => {
+  useEffect(() => {
+    const closeAllTooltip = (e: Event) => {
+      const target = e.target as HTMLElement;
+      document.querySelectorAll('[data-tooltip]').forEach((elem) => {
+        if (elem !== target.parentElement) elem.removeAttribute('open');
+      });
+    };
+
+    window.addEventListener('click', closeAllTooltip);
+
+    return () => {
+      window.removeEventListener('click', closeAllTooltip);
+    };
+  }, []);
+
+  return (
+    <>
+      <h3>
+        #3. React<sub>html details 태그 사용</sub>
+      </h3>
+      {data.map((d) => (
+        <Tooltip {...d} key={d.id} />
+      ))}
+    </>
+  );
+};
+
+export default Tooltip3;
--- Frontend/React/Inflearn/ui-study-start/src/components/tooltip/index.tsx
+++ Frontend/React/Inflearn/ui-study-start/src/components/tooltip/index.tsx
@@ -1,6 +1,6 @@
 import Tooltip1 from './1_r';
 import Tooltip2 from './2_r';
-// import Tooltip3 from './3_r'
+import Tooltip3 from './3_r';
 // import Tooltip4 from './4_r'
 // import Tooltip5V from './5_v'
 import cx from './cx';
@@ -11,7 +11,7 @@ const Tooltips = () => {
       <h2>툴팁</h2>
       <Tooltip1 />
       <Tooltip2 />
-      {/*<Tooltip3 />*/}
+      <Tooltip3 />
       {/*<Tooltip4 />*/}
       {/*<Tooltip5V />*/}
     </div>
