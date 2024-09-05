ef16fe67fa5c7588eb54dc429e4429caf24d51ab
ubardev <bellin83@gmail.com>
2024-09-06 08:24:35 +0900

    :necktie: Feat: prettier설정 추가

--- Frontend/React/Inflearn/onebite-next/section02/src/pages/_app.tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/pages/_app.tsx
@@ -2,7 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <header>Header</header>
-    <Component {...pageProps} /></>;
+  return (
+    <>
+      <header>글로벌 헤드</header>
+      <Component {...pageProps} />
+    </>
+  );
 }
