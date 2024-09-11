5b0c5c756bb798646778bd754e662c2cca49e078
ubardev <bellin83@gmail.com>
2024-09-12 06:52:57 +0900

    :necktie: 2.4) 프리페치

--- Frontend/React/Inflearn/onebite-next/section02/src/pages/_app.tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -9,12 +10,18 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push("/test");
   };
 
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
