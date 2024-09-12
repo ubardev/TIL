291ba221aa6197027e2f042be4fc2eed61b45a48
ubardev <bellin83@gmail.com>
2024-09-13 07:17:28 +0900

    :necktie: Feat: 2.6) 스타일링

--- Frontend/React/Inflearn/onebite-next/section02/src/pages/index.tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/pages/index.tsx
@@ -1,3 +1,10 @@
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>인덱스</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>인덱스</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
