c9513e76268e8cdc9910d69fa6d4a3bfc815d04d
ubardev <bellin83@gmail.com>
2024-09-13 07:45:07 +0900

    :necktie: Feat: 2.7) 글로벌 레이아웃 설정하기

---
+++ Frontend/React/Inflearn/onebite-next/section02/src/components/global-layout.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import style from "./global-layout.module.css";
+
+export default function GlobalLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}>제작 @colorfulDark</footer>
+    </div>
+  );
+}
