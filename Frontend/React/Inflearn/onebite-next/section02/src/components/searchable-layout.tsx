2afb5769c8ade27b6ca5bee3a8e4bd6af216f486
ubardev <bellin83@gmail.com>
2024-09-17 22:25:27 +0900

    :necktie: Feat: 2.8) 페이지별 레이아웃 설정하기

--- Frontend/React/Inflearn/onebite-next/section02/src/components/searchable-layout.tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/components/searchable-layout.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import style from "./searchable-layout.module.css";
+
 export default function SearchableLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const q = router.query.q as string;
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div>
-      <div>임시 서치바</div>
+      <div className={style.searchbar_container}>
+        <input
+          value={search}
+          onKeyDown={onKeyDown}
+          onChange={onChangeSearch}
+          placeholder="검색어를 입력하세요."
+        />
+        <button onClick={onSubmit}>검색</button>
+      </div>
       {children}
     </div>
   );
