11e2d3bafdcb4728e3a66abde83b225907cd3045
ubardev <bellin83@gmail.com>
2024-09-18 15:49:29 +0900

    :necktie: Feat: books 상세 퍼블리싱 추가

--- Frontend/React/Inflearn/onebite-next/section02/src/pages/index.tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/pages/index.tsx
@@ -1,13 +1,25 @@
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Home() {
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
--- Frontend/React/Inflearn/onebite-next/section02/src/pages/search/index.tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/pages/search/index.tsx
@@ -1,13 +1,17 @@
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-
-  const { q } = router.query;
-
-  return <h1>Search {q}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
