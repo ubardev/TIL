be6ce28f6e17282feddb11319fa31a440925af8a
ubardev <bellin83@gmail.com>
2024-09-18 21:52:43 +0900

    :necktie: Feat: 2.9)한입북스 UI 구현하기

--- Frontend/React/Inflearn/onebite-next/section02/src/pages/book/[id].tsx
+++ Frontend/React/Inflearn/onebite-next/section02/src/pages/book/[id].tsx
@@ -17,20 +17,20 @@ export default function Page() {
     mockData;
 
   return (
-    <div>
+    <div className={style.container}>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
         <img src={coverImgUrl} />
       </div>
-      <div>{title}</div>
-      <div>{subTitle}</div>
-      <div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
         {author} | {subTitle}
       </div>
 
-      <div>{description}</div>
+      <div className={style.description}>{description}</div>
     </div>
   );
 }
