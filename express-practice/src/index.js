c8da507c77d433945a47abd9e9c9c5614beae639
ubardev <bellin83@gmail.com>
2024-09-19 09:09:09 +0900

    :necktie: Express 기본 모듈 및 자주 사용되는 라이브러리

--- express-practice/src/index.js
+++ express-practice/src/index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dayjs from "dayjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+app.use(cors());
+app.use(helmet());
+
+const today = new Date();
+const todayToDayjs = dayjs(today).format("YYYY-MM-DD");
+console.log("todayToDayjs =====> ", todayToDayjs);
+
+const password = "1234";
+const hashedPassword = bcrypt.hashSync(password, 10);
+console.log("hashedPassword >>>>>>>>>> ", hashedPassword);
+
+const token = jwt.sign("1234", "secretKey");
+console.log("token >>>>>>>>>> ", token);
 
 // req : 요청 -> Request
 // res : 응답 -> Response
