c4b42c026d9eb1c2fa3fe5f1b087050c6bf18a94
ubardev <bellin83@gmail.com>
2024-09-24 08:54:23 +0900

    :necktie: Feat: 스웨거 기본 환경 셋팅

---
+++ express-practice/src/swagger/index.js
@@ -0,0 +1,6 @@
+import * as Swagger from "../controllers/swagger";
+console.log("Swagger >>>>>>>>>> ", Swagger);
+
+// 1) 가공하는 코드
+// 2) 스웨거에 등록할 json 만들기 defaultSwagger + 1)에서 가공한 paths
+// 3) 스웨거에 등록하는 방법
