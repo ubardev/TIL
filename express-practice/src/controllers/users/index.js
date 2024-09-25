eee379f7d5bb1e4b657d8aed5553e89983f1613c
ubardev <bellin83@gmail.com>
2024-09-26 07:33:51 +0900

    :necktie: Feat: DTO 작성하기

--- express-practice/src/controllers/users/index.js
+++ express-practice/src/controllers/users/index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { UserDTO, CreateUserDTO } from "./dto";
 
 // Router
 class UserController {
   router;
   path = "/users";
-  users = [{ id: 1, name: "ubar", age: 12 }];
+  users = [{ id: 1, firstName: "JongIn", lastName: "Hwang", age: 12 }];
   constructor() {
     this.router = Router();
     this.init();
@@ -13,12 +14,14 @@ class UserController {
   init() {
     this.router.get("/", this.getUsers.bind(this));
     this.router.get("/detail/:id", this.getUser.bind(this));
+    this.router.get("/detail/:id/fullName", this.getUserFullName.bind(this));
     this.router.post("/", this.createUser.bind(this));
   }
 
   getUsers(req, res, next) {
     try {
-      res.status(200).json({ users: this.users });
+      const users = this.users.map((user) => new UserDTO(user));
+      res.status(200).json({ users });
     } catch (err) {
       next(err);
     }
@@ -27,11 +30,13 @@ class UserController {
   getUser(req, res, next) {
     try {
       const { id } = req.params;
-      const user = this.users.find((user) => user.id === Number(id));
+      const targetUser = this.users.find((user) => user.id === Number(id));
 
-      if (!user) {
+      if (!targetUser) {
         throw { status: 404, message: "사용자를 찾을 수 없습니다." };
       }
+
+      const user = new UserDTO(targetUser);
 
       res.status(200).json({ user });
     } catch (err) {
@@ -39,19 +44,35 @@ class UserController {
     }
   }
 
+  getUserFullName(req, res, next) {
+    try {
+      const { id } = req.params;
+      const targetUser = this.users.find((user) => user.id === Number(id));
+
+      if (!targetUser) {
+        throw { status: 404, message: "사용자를 찾을 수 없습니다." };
+      }
+
+      const user = new UserDTO(targetUser);
+
+      res.status(200).json({ fullName: user.getFullName() });
+    } catch (err) {
+      next(err);
+    }
+  }
+
   createUser(req, res, next) {
     try {
-      const { name, age } = req.body;
+      const { firstName, lastName, age } = req.body;
 
-      if (!name) {
+      if (!firstName || !lastName) {
         throw { status: 400, message: "이름이 없습니다." };
       }
 
-      this.users.push({
-        id: new Date().getTime(),
-        name,
-        age,
-      });
+      const user = new CreateUserDTO(firstName, lastName, age);
+      const newUser = user.getNewUser();
+
+      this.users.push(newUser);
 
       res.status(201).json({ users: this.users });
     } catch (err) {
