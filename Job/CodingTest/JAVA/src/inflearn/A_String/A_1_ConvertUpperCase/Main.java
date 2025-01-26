3393c661f3954dd1eae4da08120c0e9783d49d49
ubardev <bellin83@gmail.com>
2025-01-27 00:51:25 +0900

    :necktie: Feat: 대소문자 변환 풀이  - 2

--- Job/CodingTest/JAVA/src/inflearn/A_String/A_1_ConvertUpperCase/Main.java
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_1_ConvertUpperCase/Main.java
@@ -6,8 +6,13 @@ public class Main {
         String answer = "";
 
         for (char x : str.toCharArray()) {
-            if (Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-            else answer += Character.toLowerCase(x);
+//            if (Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+//            else answer += Character.toLowerCase(x);
+            if (x >= 97 && x <=122) {
+                answer += (char)(x - 32);
+            } else {
+                answer += (char)(x + 32);
+            }
         }
 
         return answer;
