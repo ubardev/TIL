5171a47ac13bc4fcab50a358384290d0a74abb34
ubardev <bellin83@gmail.com>
2025-01-27 00:29:45 +0900

    :necktie: Feat: 1. 문자 찾기 풀이 -1

---
+++ Job/CodingTest/JAVA/src/inflearn/Sample.java
@@ -0,0 +1,18 @@
+package inflearn;
+
+import java.util.*;
+
+public class Sample {
+    public int solution(String str, char t) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sample T = new Sample();
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
