70b9df296aab650065cc4295518d8bdd2f4f7ae4
ubardev <bellin83@gmail.com>
2025-01-27 15:24:14 +0900

    :necktie: Feat: 문장 속 단어 풀이 - 2

--- Job/CodingTest/JAVA/src/inflearn/A_String/A_3/Main.java
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_3/Main.java
@@ -3,26 +3,45 @@ package inflearn.A_String.A_3;
 import java.util.*;
 
 public class Main {
+//    public String solution(String str) {
+//        String answer = "";
+//        int m = Integer.MIN_VALUE;
+//        String[] s = str.split(" ");
+//
+//        for (String x : s) {
+//            int len  = x.length();
+//            if (len > m) {
+//                m = len;
+//                answer = x;
+//            }
+//        }
+//
+//        return answer;
+//    }
     public String solution(String str) {
         String answer = "";
-        int m = Integer.MIN_VALUE;
-        String[] s = str.split(" ");
+        int m = Integer.MIN_VALUE, pos;
 
-        for (String x : s) {
-            int len  = x.length();
+        while((pos = str.indexOf(" ")) != -1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+
             if (len > m) {
                 m = len;
-                answer = x;
+                answer = tmp;
             }
+
+            str = str.substring(pos + 1);
         }
 
+        if(str.length() > m) answer = str;
         return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.nextLine();
+        String str = kb.nextLine(); //it is time to study
         System.out.println(T.solution(str));
     }
 }
