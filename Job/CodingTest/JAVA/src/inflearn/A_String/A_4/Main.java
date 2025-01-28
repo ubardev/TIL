193cd0dbf1bdbdd5f1ce4b4c993dfe39b0f8bec2
ubardev <bellin83@gmail.com>
2025-01-29 00:56:14 +0900

    :necktie: Feat: 단어 뒤집기 - 2

--- Job/CodingTest/JAVA/src/inflearn/A_String/A_4/Main.java
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_4/Main.java
@@ -4,11 +4,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+//    public ArrayList<String> solution(int n, String[] str) {
+//        ArrayList<String> answer = new ArrayList<>();
+//
+//        for(String x : str) {
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+//
+//        return answer;
+//    }
+
     public ArrayList<String> solution(int n, String[] str) {
         ArrayList<String> answer = new ArrayList<>();
 
         for(String x : str) {
-            String tmp = new StringBuilder(x).reverse().toString();
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+
+            while(lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+
+            String tmp = String.valueOf(s);
             answer.add(tmp);
         }
 
