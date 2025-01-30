8243c2661920db994faa5f294d9cd09720f6dad7
ubardev <bellin83@gmail.com>
2025-01-31 07:08:24 +0900

    :necktie: Feat: 6. 중복 문자 제거 풀이

---
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_6/Main.java
@@ -0,0 +1,26 @@
+package inflearn.A_String.A_6;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+//            System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    // ksekkset
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
