820abf15a9e9680702079e06631409e68e49914d
ubardev <bellin83@gmail.com>
2025-01-31 23:43:14 +0900

    :necktie: Feat: 8. 팰린드롬 풀이 추가

---
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_8/Main.java
@@ -0,0 +1,23 @@
+package inflearn.A_String.A_8;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution(String s) {
+        String answer = "NO";
+        s = s.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(s).reverse().toString();
+
+        if (s.equals(tmp)) answer = "YES";
+
+        return answer;
+    }
+
+    // found7, time: study; Yduts; emit, 7Dnuof
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
