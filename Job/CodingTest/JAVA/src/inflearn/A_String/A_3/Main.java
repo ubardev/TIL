f82e87ded87dc85ff8b20084d927d5019b4b0967
ubardev <bellin83@gmail.com>
2025-02-19 00:46:43 +0900

    :necktie: Feat: Split 수동으로 풀이

--- Job/CodingTest/JAVA/src/inflearn/A_String/A_3/Main.java
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_3/Main.java
@@ -3,19 +3,39 @@ package inflearn.A_String.A_3;
 import java.util.Scanner;
 
 public class Main {
+//  public String solution(String str) {
+//    String answer = "";
+//    int min = Integer.MIN_VALUE;
+//    String[] strList = str.split(" ");
+//
+//    for (String word : strList) {
+//      int length = word.length();
+//      if (length > min) {
+//        min = length;
+//        answer = word;
+//      }
+//    }
+//
+//    return answer;
+//  }
+
   public String solution(String str) {
     String answer = "";
-    int min = Integer.MIN_VALUE;
-    String[] strList = str.split(" ");
+    int m = Integer.MIN_VALUE, pos;
 
-    for (String word : strList) {
-      int length = word.length();
-      if (length > min) {
-        min = length;
-        answer = word;
+    while((pos = str.indexOf(" ")) != -1) {
+      String tmp = str.substring(0, pos);
+      int len = tmp.length();
+
+      if (len > m) {
+        m = len;
+        answer = tmp;
       }
+
+      str = str.substring(pos + 1);
     }
 
+    if (str.length() > m) answer = str;
     return answer;
   }
 
