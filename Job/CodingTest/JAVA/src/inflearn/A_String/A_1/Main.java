f4061c96146d06d015736037942a2141f8fc7315
ubardev <bellin83@gmail.com>
2025-03-17 01:11:50 +0900

    :necktie: Feat: A_1 다시 풀이

--- Job/CodingTest/JAVA/src/inflearn/A_String/A_1/Main.java
+++ Job/CodingTest/JAVA/src/inflearn/A_String/A_1/Main.java
@@ -20,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+        String str = kb.nextLine();
         System.out.println(T.solution(str));
     }
 }
