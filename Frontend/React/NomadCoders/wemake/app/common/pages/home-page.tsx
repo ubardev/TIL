ee9e33fb7087daa54ec39e9cfca4380b3c606255
ubardev <bellin83@gmail.com>
2025-03-24 22:59:41 +0900

    :necktie: Feat: #3.6 Post Card
    햐 세ㅕ노

--- Frontend/React/NomadCoders/wemake/app/common/pages/home-page.tsx
+++ Frontend/React/NomadCoders/wemake/app/common/pages/home-page.tsx
@@ -1,27 +1,18 @@
 import { Link, type MetaFunction } from "react-router";
-import {
-  Card,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../components/ui/card";
-import { ChevronUpIcon, EyeIcon, MessageCircleIcon } from "lucide-react";
-import { Button } from "../components/ui/button";
 import { ProductCard } from "~/features/products/components/product-card";
+import { Button } from "../components/ui/button";
+import { PostCard } from "~/features/community/components/post-card";
 
 export const meta: MetaFunction = () => {
   return [
-    {
-      title: "Home | wemake",
-    },
+    { title: "Home | wemake" },
     { name: "description", content: "Welcome to wemake" },
   ];
 };
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -30,8 +21,11 @@ export default function HomePage() {
           <p className="text-xl font-light text-foreground">
             The best products made by our community today.
           </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/products/leaderboards">Explore all products &rarr;</Link>
+          </Button>
         </div>
-        {Array.from({ length: 10 }).map((_, index) => (
+        {Array.from({ length: 11 }).map((_, index) => (
           <ProductCard
             id={`productId-${index}`}
             name="Product Name"
@@ -39,6 +33,29 @@ export default function HomePage() {
             commentsCount={12}
             viewsCount={12}
             votesCount={120}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Discussions
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The latest discussions from our community.
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <PostCard
+            id={`postId-${index}`}
+            title="What is the best productivity tool?"
+            author="Nico"
+            authorAvatarUrl="https://github.com/apple.png"
+            category="Productivity"
+            postedAt="12 hours ago"
           />
         ))}
       </div>
---
+++ Frontend/React/NomadCoders/wemake/app/features/community/components/post-card.tsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router";
+import {
+  Card,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
+import { Avatar, AvatarImage } from "~/common/components/ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
+import { Button } from "~/common/components/ui/button";
+
+interface PostCardProps {
+  id: string;
+  title: string;
+  author: string;
+  authorAvatarUrl: string;
+  category: string;
+  postedAt: string;
+}
+
+export function PostCard({
+  id,
+  title,
+  author,
+  authorAvatarUrl,
+  category,
+  postedAt,
+}: PostCardProps) {
+  return (
+    <Link to={`/community/${id}`}>
+      <Card className="bg-transparent hover:bg-card/50 transition-colors">
+        <CardHeader className="flex flex-row items-center gap-2">
+          <Avatar className="size-14">
+            <AvatarFallback>{author[0]}</AvatarFallback>
+            {authorAvatarUrl && <AvatarImage src={authorAvatarUrl} />}
+          </Avatar>
+          <div className="space-y-2">
+            <CardTitle>{title}</CardTitle>
+            <div className="flex gap-2 text-sm leading-tight text-muted-foreground">
+              <span>
+                {author} on {category}
+              </span>
+              <span>·</span>
+              <span>{postedAt}</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex justify-end">
+          <Button variant="link" asChild>
+            <Link to={`/community/${id}`}>Reply &rarr;</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+}
