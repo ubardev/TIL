50c00ebd37afd0f78acd4c5195d4f150a80dce18
ubardev <bellin83@gmail.com>
2025-03-28 03:39:19 +0900

    :necktie: Feat: 4.피보나치 수열 재풀이

--- Job/CodingTest/JAVA/src/inflearn/B_Array/B_4/Main.java
+++ Job/CodingTest/JAVA/src/inflearn/B_Array/B_4/Main.java
@@ -2,36 +2,25 @@ package inflearn.B_Array.B_4;
 
 import java.util.Scanner;
 
-public class Main {
-//    public int[] solution(int n) {
-//        int[] answer = new int[n];
-//
-//        answer[0] = 1;
-//        answer[1] = 1;
-//
-//        for (int i = 2; i < n; i++) {
-//            answer[i] = answer[i - 2] + answer[i - 1];
-//        }
-//
-//        return answer;
-//    }
-
+class Main {
     public void solution(int n) {
-        int a = 1, b = 1, c;
-        System.out.print(a + " " + b + " ");
+        int first = 1, second = 1, third;
+
+        System.out.print(first + " " + second + " ");
+
         for (int i = 2; i < n; i++) {
-            c = a + b;
-            System.out.print(c + " ");
-            a = b;
-            b = c;
+            third = first + second;
+            System.out.print(third + " ");
+            first = second;
+            second = third;
         }
+
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-//        for (int x : T.solution(n)) System.out.print(x + " ");
         T.solution(n);
     }
 }
---
+++ Job/CodingTest/JAVA/src/inflearn/B_Array/B_4/Solution.java
@@ -0,0 +1,37 @@
+package inflearn.B_Array.B_4;
+
+import java.util.Scanner;
+
+public class Solution {
+//    public int[] solution(int n) {
+//        int[] answer = new int[n];
+//
+//        answer[0] = 1;
+//        answer[1] = 1;
+//
+//        for (int i = 2; i < n; i++) {
+//            answer[i] = answer[i - 2] + answer[i - 1];
+//        }
+//
+//        return answer;
+//    }
+
+    public void solution(int n) {
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution T = new Solution();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+//        for (int x : T.solution(n)) System.out.print(x + " ");
+        T.solution(n);
+    }
+}
