3f64d2ef6308fbc1e4c9f45df51af4b4b0601310
ubardev <bellin83@gmail.com>
2025-04-22 00:48:01 +0900

    :necktie: Feat: 05. 합이 같은 부분집합 인프런 풀이 추가

--- Job/CodingTest/Javascript/Inflearn/08-재귀함수와완전탐색/05-합이같은부분집합(이진트리DFS).js
+++ Job/CodingTest/Javascript/Inflearn/08-재귀함수와완전탐색/05-합이같은부분집합(이진트리DFS).js
@@ -1,3 +1,7 @@
+// inflearn
+// function solution(numbers) {}
+
+// GPT
 // function solution(numbers) {
 //   const numberCount = numbers.length;
 //   let isPossible = false;
@@ -25,29 +29,24 @@
 
 
 function solution(numbers) {
+  let answer = "NO";
+  const totalSum = numbers.reduce((acc, number) => acc + number, 0);
   const numberCount = numbers.length;
-  let isPossible = false;
 
-  function DFS(level, sum, subset) {
-    if (isPossible) return;
+  function DFS(level, sum) {
+    if (answer === "YES") return;
 
     if (level === numberCount) {
-      const remainingNumbers = numbers.filter((_, index) => !subset.includes(index));
-      const remainingSum = remainingNumbers.reduce((acc, num) => acc + num, 0);;
-
-      if (sum === remainingSum) {
-        isPossible = true;
-      }
+      if ((totalSum - sum) === sum) answer = "YES";
       return;
     }
 
-    DFS(level + 1, sum + numbers[level], [...subset, numbers[level]]);
-    DFS(level + 1, sum, subset);
+    DFS(level + 1, sum + numbers[level]);
+    DFS(level + 1, sum);
   }
 
-  DFS(0, 0, []);
-
-  return isPossible ? "YES" : "NO";
+  DFS(0, 0);
+  return answer;
 }
 
 const input = [1, 3, 5, 6, 7, 10];
