f28975746953db30d5cbad40e0cc67e72781bdb7
ubardev <bellin83@gmail.com>
2025-10-28 00:11:55 +0900

    👔 Feat: Tailwind CSS 핵심정리 2

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -21,6 +21,19 @@ function App() {
       <div className="mx-5 my-5 h-50 w-50 bg-red-400 px-5 py-5">
         <div className="h-full w-full bg-blue-400"></div>
       </div>
+
+      {/* 5. 보더 */}
+      <div className="m-5 rounded-md border-t-2 border-r-2 border-b-2 border-l-2 border-red-500">
+        border
+      </div>
+
+      {/* 6. 플렉스 컨테이너 */}
+      <div className="flex flex-row items-start justify-evenly">
+        <div className="h-10 w-10 border">1</div>
+        <div className="h-20 w-10 flex-1 border">2</div>
+        <div className="h-30 w-10 border">3</div>
+        <div className="h-40 w-10 border">4</div>
+      </div>
     </div>
   );
 }
