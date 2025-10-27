c92be2b8fc775755b83ea102476800220c3a9e9d
ubardev <bellin83@gmail.com>
2025-10-27 23:13:28 +0900

    ➕ Tailwind 설정 추가

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Hello World</div>;
+  return (
+    <div className="bg-black text-2xl font-bold underline">Hello World</div>
+  );
 }
 
 export default App;
