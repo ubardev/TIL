6a52aac71a641a52a4046a016b1e44755aa68855
ubardev <bellin83@gmail.com>
2025-10-27 23:59:36 +0900

    👔 Feat: Tailwind CSS 핵심정리 1

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -2,7 +2,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="bg-black text-2xl font-bold underline">Hello World</div>
+    <div>
+      {/* 1. 타이포그래프 */}
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
+      <div className="text-lg font-bold">text-lg</div>
+      <div className="text-xl font-extrabold">text-xl</div>
+      <div className="text-2xl font-black">text-2xl</div>
+      <div className="text-[13px]">text-13px</div>
+
+      {/* 2. 백그라운드컬러 */}
+      <div className="bg-amber-500">amber-500</div>
+
+      {/* 3. 사이즈 */}
+      <div className="h-20 w-full bg-blue-500">box</div>
+
+      {/* 4. 여백 */}
+      <div className="mx-5 my-5 h-50 w-50 bg-red-400 px-5 py-5">
+        <div className="h-full w-full bg-blue-400"></div>
+      </div>
+    </div>
   );
 }
 
