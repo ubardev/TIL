5fe85b9ea10e979988db8992689422e78d545b37
ubardev <bellin83@gmail.com>
2025-11-02 00:01:43 +0900

    👔 Feat: Shadcn 추가 설치 프로그램 설치

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -1,10 +1,46 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Toaster } from "@/components/ui/sonner";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+import { toast } from "sonner";
 
 function App() {
+  const isActive = false;
+
   return (
-    <div>
-      <Button>버튼!</Button>
+    <div className="p-5">
+      <Toaster />
+
+      <Textarea />
+
+      <Input placeholder="입력 ..." />
+
+      <Button
+        onClick={() => {
+          toast("토스트 메세지", {
+            position: "top-center",
+          });
+        }}
+      >
+        버튼!
+      </Button>
+      <Button variant={"destructive"}>버튼</Button>
+      <Button variant={"ghost"}>버튼</Button>
+      <Button variant={"link"}>버튼</Button>
+      <Button variant={"outline"}>버튼</Button>
+      <Button variant={"secondary"}>버튼</Button>
+
+      <div
+        className={cn(
+          "w-10 text-sm",
+          isActive ? "text-green-500" : "text-red-500",
+        )}
+      >
+        isActive
+      </div>
+
       <div className="text-primary">Primary</div>
       <div className="text-muted">Muted</div>
       <div className="text-destructive">Destructive</div>
