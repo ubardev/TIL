acb06ce72908b428c4ca7b39627993b2caf3917c
ubardev <bellin83@gmail.com>
2025-11-01 23:18:01 +0900

    👔 Feat: shadcn 설치

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -1,39 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
-      {/* 1. 타이포그래프 */}
-      <div className="text-xs text-red-500">text-xs</div>
-      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
-      <div className="text-lg font-bold">text-lg</div>
-      <div className="text-xl font-extrabold">text-xl</div>
-      <div className="text-2xl font-black">text-2xl</div>
-      <div className="text-[13px]">text-13px</div>
-
-      {/* 2. 백그라운드컬러 */}
-      <div className="bg-amber-500">amber-500</div>
-
-      {/* 3. 사이즈 */}
-      <div className="h-20 w-full bg-blue-500">box</div>
-
-      {/* 4. 여백 */}
-      <div className="mx-5 my-5 h-50 w-50 bg-red-400 px-5 py-5">
-        <div className="h-full w-full bg-blue-400"></div>
-      </div>
-
-      {/* 5. 보더 */}
-      <div className="m-5 rounded-md border-t-2 border-r-2 border-b-2 border-l-2 border-red-500">
-        border
-      </div>
-
-      {/* 6. 플렉스 컨테이너 */}
-      <div className="flex flex-row items-start justify-evenly">
-        <div className="h-10 w-10 border">1</div>
-        <div className="h-20 w-10 flex-1 border">2</div>
-        <div className="h-30 w-10 border">3</div>
-        <div className="h-40 w-10 border">4</div>
-      </div>
+      <Button>버튼!</Button>
+      <div className="text-primary">Primary</div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
     </div>
   );
 }
