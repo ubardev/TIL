aea372c1abdd87244343cff2680bb6a3c52c8db2
ubardev <bellin83@gmail.com>
2025-11-02 22:53:13 +0900

    👔 Feat: Shadcn/ui 핵심정리 2

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -1,16 +1,95 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Toaster } from "@/components/ui/sonner";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 import { toast } from "sonner";
+import { ChefHat } from "lucide-react";
 
 function App() {
   const isActive = false;
 
   return (
     <div className="p-5">
+      <ChefHat className="h-10 w-10 fill-red-500" />
+      <AlertDialog>
+        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Title</AlertDialogTitle>
+          <AlertDialogDescription>Description</AlertDialogDescription>
+          <div>body</div>
+          <div>
+            <AlertDialogAction
+              onClick={() => {
+                console.log("Action!");
+              }}
+            >
+              Action
+            </AlertDialogAction>
+            <AlertDialogCancel onClick={() => console.log("Cancel")}>
+              Cancel
+            </AlertDialogCancel>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Dialog>
+        <DialogTrigger>Open Dialog</DialogTrigger>
+        <DialogContent>
+          <DialogTitle>Title</DialogTitle>
+          <div>Body</div>
+        </DialogContent>
+      </Dialog>
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>Open</Button>
+        </PopoverTrigger>
+        <PopoverContent>Content!</PopoverContent>
+      </Popover>
+
+      <Carousel className="mx-10">
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">1</CarouselItem>
+          <CarouselItem className="basis-1/3">2</CarouselItem>
+          <CarouselItem className="basis-1/3">3</CarouselItem>
+          <CarouselItem className="basis-1/3">4</CarouselItem>
+          <CarouselItem className="basis-1/3">5</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
       <Toaster />
 
       <Textarea />
