045e6ec4aa4ef9e73e9729c05ae993530d4142dc
ubardev <bellin83@gmail.com>
2025-11-02 23:53:18 +0900

    👔 Feat: (2.7) React Router V7 핵심정리

--- Frontend/React/Inflearn/onebite-exam/src/App.tsx
+++ Frontend/React/Inflearn/onebite-exam/src/App.tsx
@@ -1,129 +1,28 @@
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import { Outlet, Route, Routes } from "react-router";
 import "./App.css";
-import { Button } from "@/components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Toaster } from "@/components/ui/sonner";
-import { Textarea } from "@/components/ui/textarea";
-import { cn } from "@/lib/utils";
-import { toast } from "sonner";
-import { ChefHat } from "lucide-react";
+import IndexPage from "@/pages/index-page";
+import SignInPage from "@/pages/sign-in-page";
+import SignUpPage from "@/pages/sign-up-page";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
-  const isActive = false;
-
   return (
-    <div className="p-5">
-      <ChefHat className="h-10 w-10 fill-red-500" />
-      <AlertDialog>
-        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogTitle>Title</AlertDialogTitle>
-          <AlertDialogDescription>Description</AlertDialogDescription>
-          <div>body</div>
-          <div>
-            <AlertDialogAction
-              onClick={() => {
-                console.log("Action!");
-              }}
-            >
-              Action
-            </AlertDialogAction>
-            <AlertDialogCancel onClick={() => console.log("Cancel")}>
-              Cancel
-            </AlertDialogCancel>
-          </div>
-        </AlertDialogContent>
-      </AlertDialog>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
 
-      <Dialog>
-        <DialogTrigger>Open Dialog</DialogTrigger>
-        <DialogContent>
-          <DialogTitle>Title</DialogTitle>
-          <div>Body</div>
-        </DialogContent>
-      </Dialog>
-
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button>Open</Button>
-        </PopoverTrigger>
-        <PopoverContent>Content!</PopoverContent>
-      </Popover>
-
-      <Carousel className="mx-10">
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">1</CarouselItem>
-          <CarouselItem className="basis-1/3">2</CarouselItem>
-          <CarouselItem className="basis-1/3">3</CarouselItem>
-          <CarouselItem className="basis-1/3">4</CarouselItem>
-          <CarouselItem className="basis-1/3">5</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-
-      <Toaster />
-
-      <Textarea />
-
-      <Input placeholder="입력 ..." />
-
-      <Button
-        onClick={() => {
-          toast("토스트 메세지", {
-            position: "top-center",
-          });
-        }}
-      >
-        버튼!
-      </Button>
-      <Button variant={"destructive"}>버튼</Button>
-      <Button variant={"ghost"}>버튼</Button>
-      <Button variant={"link"}>버튼</Button>
-      <Button variant={"outline"}>버튼</Button>
-      <Button variant={"secondary"}>버튼</Button>
-
-      <div
-        className={cn(
-          "w-10 text-sm",
-          isActive ? "text-green-500" : "text-red-500",
-        )}
-      >
-        isActive
-      </div>
-
-      <div className="text-primary">Primary</div>
-      <div className="text-muted">Muted</div>
-      <div className="text-destructive">Destructive</div>
-    </div>
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
